cf3e3f646db9d7e25d10c1afb956f86a7967df3b
satendra.singh <satendra.singh@seagate.com>
2020-03-20 00:37:22 -0600

    EOS-5762: Removed m0init() and m0fini() from utils.

--- src/common/utils.c
+++ src/common/utils.c
@@ -21,7 +21,6 @@
 #include <common/helpers.h>   /*RC_WRAP_LABEL*/
 #include <str.h>
 #include <ini_config.h>
-#include <m0common.h>
 
 static int utils_initialized;
 
@@ -35,19 +34,11 @@ void buff_init(buff_t *dest, void *src, size_t src_len)
 int utils_init(struct collection_item *cfg_items)
 {
         int rc = 0;
-	
 	dassert(utils_initialized == 0);
         utils_initialized++;
 
 	/*TODO Placeholder, conf_init needs to be finalize*/
 
- 	rc = m0init(cfg_items);
-	if (rc != 0) {
-		log_err("m0init failed, rc=%d ", rc);
-                goto err;
-        }
-
-err:
         return rc;
 }
 
@@ -55,8 +46,6 @@ err:
 int utils_fini(void)
 {
 	int rc = 0;
-	m0fini();
-
 	/*TODO conf_fini*/
 	return rc;
 }
