e4da75460b2363f2554b074f7c923a59f1a2159f
Shailesh Vaidya <shailesh.vaidya@seagate.com>
2020-07-07 04:47:25 -0600

    Added changelog.sh script

--- scripts/release_support/changelog.sh
+++ scripts/release_support/changelog.sh
@@ -2,6 +2,9 @@
 
 START_BUILD=$1
 TARGET_BUILD=$2
+BUILD_LOCATION=$3
+GIT_CRED=$4
+
 
 function usage() {
 echo "No inputs provided exiting..."
@@ -17,13 +20,13 @@ fi
 if [ -z $START_BUILD ]; then echo "No START_BUILD provided.."; exit 1 ; fi
 if [ -z $TARGET_BUILD ]; then echo "No TARGET_BUILD provided.."; exit 1; fi
 
-declare -A COMPONENT_LIST=( [eos-s3server]='http://gerrit.mero.colo.seagate.com:8080/s3server'
-[eos-core]='http://gerrit.mero.colo.seagate.com:8080/mero'
-[eos-hare]='http://gitlab.mero.colo.seagate.com/mero/hare'
-[eos-prvsnr]='http://gitlab.mero.colo.seagate.com/eos/provisioner/ees-prvsnr'
-[eos-sspl]='http://gerrit.mero.colo.seagate.com:8080/sspl'
-[eos-csm_agent]='http://gitlab.mero.colo.seagate.com/eos/csm'
- )
+declare -A COMPONENT_LIST=( [eos-s3server]="https://$GIT_CRED@github.com/Seagate/cortx-s3server.git"
+[eos-core]="https://$GIT_CRED@github.com/Seagate/cortx-motr.git"
+[eos-hare]="http://gitlab.mero.colo.seagate.com/mero/hare.git"
+[eos-prvsnr]="https://$GIT_CRED@github.com/Seagate/cortx-prvsnr.git"
+[eos-sspl]="https://$GIT_CRED@github.com/Seagate/cortx-sspl.git"
+[eos-csm_agent]="https://$GIT_CRED@github.com/Seagate/cortx-csm.git"
+)
 
 clone_dir="/root/git_build_checkin_stats"
 time_zone="Asia/Calcutta"
@@ -35,15 +38,20 @@ export TZ=$time_zone;ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ 
 
 pushd $clone_dir/clone
 
-wget -q http://ci-storage.mero.colo.seagate.com/releases/eos/Cortx-v1.0.0_Beta/rhel-7.7.1908/$START_BUILD/prod/MANIFEST.MF -O start_build_manifest.txt
-wget -q http://ci-storage.mero.colo.seagate.com/releases/eos/Cortx-v1.0.0_Beta/rhel-7.7.1908/$TARGET_BUILD/prod/MANIFEST.MF -O target_build_manifest.txt
+wget -q $BUILD_LOCATION/$START_BUILD/dev/MANIFEST.MF -O start_build_manifest.txt
+wget -q $BUILD_LOCATION/$TARGET_BUILD/dev/MANIFEST.MF -O target_build_manifest.txt
 
 for component in "${!COMPONENT_LIST[@]}"
 do
         echo "Component:$component"
         echo "Repo:${COMPONENT_LIST[$component]}"
          dir=$(echo ${COMPONENT_LIST[$component]} |  awk -F'/' '{print $NF}')
-         git clone --branch Cortx-v1.0.0_Beta ${COMPONENT_LIST[$component]}.git $dir
+         git clone --branch master ${COMPONENT_LIST[$component]} $dir
+      	 rc=$?
+          if [ $rc -ne 0 ]; then 
+          echo "ERROR:git clone failed for $component"
+          exit 1
+          fi	
 
                 if [ $component == eos-hare ] || [ $component == eos-sspl ]; then
                                         start_hash=$(grep $component start_build_manifest.txt | head -1 | awk -F['_'] '{print $2}' | cut -d. -f1 |  sed 's/git//g'); echo $start_hash
@@ -59,7 +67,7 @@ do
                  pushd $dir
          echo -e "\t--[ Check-ins for $dir from $START_BUILD to $TARGET_BUILD ]--" >> $report_file
          echo -e "Githash|Description|Author|" >> $report_file
-         git log $start_hash..$target_hash --oneline --pretty=format:"%h|%s|%an|" >>  $report_file
+         git log $start_hash..$target_hash --oneline --pretty=format:"%h|%cd|%s|%an|" >>  $report_file
          echo -e "\n" >> $report_file
          echo -e "---------------------------------------------------------------------------------------------" >> $report_file
          popd
