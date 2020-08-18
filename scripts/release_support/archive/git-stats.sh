43336f58110afc19ad771c60806023ff5a853b71
Gowthaman Chinnathambi <gowthaman.chinnathambi@seagate.com>
2020-08-18 19:47:13 +0530

    EOS-12171 : Added Copyright Header for scripts/dockerfiles
    Signed-off-by: Gowthaman Chinnathambi <gowthaman.chinnathambi@seagate.com>

--- scripts/release_support/archive/git-stats.sh
+++ scripts/release_support/archive/git-stats.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
+#
+# Copyright (c) 2020 Seagate Technology LLC and/or its Affiliates
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# For any questions about this software or licensing,
+# please email opensource@seagate.com or cortx-questions@seagate.com.
+#
+
 REPO_LIST=(http://gitlab.mero.colo.seagate.com/eos/csm http://gitlab.mero.colo.seagate.com/eos/provisioner/ees-prvsnr http://gitlab.mero.colo.seagate.com/mero/hare http://gerrit.mero.colo.seagate.com:8080/sspl http://gerrit.mero.colo.seagate.com:8080/s3server http://gerrit.mero.colo.seagate.com:8080/mero)
 
 clone_dir=/root/gitstats
