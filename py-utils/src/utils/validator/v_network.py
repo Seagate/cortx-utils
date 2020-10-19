f834bc99d56ca6b079a2a851eb023b2ca9191daf
Shri Bhargav Metta <shri.metta@seagate.com>
2020-10-19 11:12:53 -0600

    Validator framework base implementation.
    Signed-off-by: Shri Bhargav Metta <shri.metta@seagate.com>

---
+++ py-utils/src/utils/validator/v_network.py
@@ -0,0 +1,54 @@
+#!/bin/env python3
+
+# CORTX-Py-Utils: CORTX Python common library.
+# Copyright (c) 2020 Seagate Technology LLC and/or its Affiliates
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+# For any questions about this software or licensing,
+# please email opensource@seagate.com or cortx-questions@seagate.com.
+
+import os
+import errno
+
+from cortx.utils.validator.error import VError
+
+class NetworkV:
+    """ Network related validations """
+    def __init__(self):
+        # TODO: Perform initialization for network level commands
+        pass
+
+    def validate(self, args):
+        """ Process network valiations """
+
+        if not isinstance(args, list) or len(args) < 2:
+            raise VError(errno.EINVAL, "Invalid parameters")
+
+        action = args[0]
+
+        # TODO Write actual code here
+        if action == "iface": 
+            self.validate_iface(args[1])
+
+    def validate_iface(self, iface):
+        # TODO write
+        
+        iface_list = os.listdir("/sys/class/net/")
+        if iface in iface_list:
+            return
+
+        raise Exception(errno.ENOENT, "No such interface %s" %iface)
+
+
+# Test Network
+if __name__ == "__main__":
+    args = ['iface', 'eth0']
+    NetworkV().validate(args)
---
+++ py-utils/src/utils/validator/validate.py
@@ -0,0 +1,61 @@
+#!/bin/env python3
+
+# CORTX-Py-Utils: CORTX Python common library.
+# Copyright (c) 2020 Seagate Technology LLC and/or its Affiliates
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+# For any questions about this software or licensing,
+# please email opensource@seagate.com or cortx-questions@seagate.com.
+
+import sys
+import errno
+import argparse
+import inspect
+import traceback
+
+from cortx.utils.validator import commands
+
+class ValidatorCommandFactory:
+    """ Factor for all kinds of validations """
+
+    @staticmethod
+    def usage(prog):
+        sys.stderr.write("usage: %s [-h] [network <args>]\n" % prog)
+
+    @staticmethod
+    def get_command(description, argv):
+        parser = argparse.ArgumentParser(description)
+
+        subparsers = parser.add_subparsers()
+        cmds = inspect.getmembers(commands, inspect.isclass)
+        for name, cmd in cmds:
+            if name != "VCommand" and "VCommand" in name:
+                cmd.add_args(subparsers)
+        args = parser.parse_args(argv)
+        return args.command(args.args)
+
+
+def main(argv):
+    try:
+        description = "CORTX Validator command"
+        command = ValidatorCommandFactory.get_command(description, argv[1:])
+
+        command.process()
+
+    except Exception as e:
+        sys.stderr.write("error: %s\n\n" %str(e))
+        sys.stderr.write("%s\n" %traceback.format_exc())
+        ValidatorCommandFactory.usage(argv[0])
+        return errno.EINVAL
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
