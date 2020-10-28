00325a7182573c876d39aedc1b395120bc58c073
Shailesh Vadiya <shailesh.vaidya@seagate.com>
2020-10-28 14:02:34 +0530

    EOS-14638 Excluded unwanted Motr and SSPL packages from RELEASE.INFO (#119)
    * EOS-14638 Excluded unwanted Motr and SSPL packages from RELEASE.INFO file
    
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>
    
    * EOS-14638 Fixed Codacy errors
    
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>
    
    * EOS-14638 Fixed Codacy errors
    
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>
    
    * EOS-14638 Fixed Codacy errors
    
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>
    
    * EOS-14638 Fixed Codacy errors
    
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>
    
    * EOS-14638 Fixed Codacy errors
    
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>

--- scripts/release_support/build_release_info.sh
+++ scripts/release_support/build_release_info.sh
@@ -19,17 +19,19 @@
 #
 
 BUILD_LOCATION=$1
+EXCLUDE_PACKAGES="cortx-motr-devel\|cortx-motr-tests-ut\|cortx-libsspl_sec-devel\|cortx-libsspl_sec-method_pki"
 echo -e "Generating RELEASE.INFO file"
-pushd $BUILD_LOCATION
+pushd "$BUILD_LOCATION" || exit
 cat <<EOF > RELEASE.INFO
 ---
 NAME: "CORTX"
 VERSION: "1.0.0"
-BUILD: $(echo $BUILD_NUMBER | sed -e 's/^/\"/g' -e 's/$/\"/g')
+BUILD: $(echo "$BUILD_NUMBER" | sed -e 's/^/\"/g' -e 's/$/\"/g')
 OS: $(cat /etc/redhat-release | sed -e 's/ $//g' -e 's/^/\"/g' -e 's/$/\"/g')
 DATETIME: $(date +"%d-%b-%Y %H:%M %Z" | sed -e 's/^/\"/g' -e 's/$/\"/g')
 KERNEL: $(ls cortx-motr-[0-9]*.rpm | sed -e  's/.*3/3/g' -e 's/.x86_64.rpm//g' -e 's/^/\"/g' -e 's/$/\"/g')
 COMPONENTS:
-$(ls -1 *.rpm | awk '{ print "    - \""$1"\""}')
+$(ls -1 ./*.rpm | sed 's/\.\///g' | grep -v "$EXCLUDE_PACKAGES" |  awk '{ print "    - \""$1"\""}')
 EOF
-popd
+popd || exit
+
