73e45fa9ecad077d13c40857cec01d708100d185
Divya Vijayakumar <divya.vijaykumar@seagate.com>
2020-11-12 15:55:37 +0530

    Network pre-checks as a new patch (#88)
    * adding network pre-checks only
    
    Signed-off-by: Divya Vijayakumar <divya.vijaykumar@seagate.com>
    
    * rebased to main and added review changes
    
    Signed-off-by: Divya Vijayakumar <divya.vijaykumar@seagate.com>
    
    * appending ip_connectivity method
    
    Signed-off-by: Divya Vijayakumar <divya.vijaykumar@seagate.com>
    
    Co-authored-by: ajaysrivas <66410865+ajaysrivas@users.noreply.github.com>

--- py-utils/src/utils/validator/v_network.py
+++ py-utils/src/utils/validator/v_network.py
@@ -31,21 +31,39 @@ class NetworkV:
         Usage (arguments to be provided):
         1. network connectivity <ip1> <ip2> <host>
         2. network passwordless <user> <node1> <node2>
+        3. network drivers <driver_name> <node1> <node2>
+        4. network hca <provider> <node1> <node2>
         """
 
         if not isinstance(args, list):
             raise VError(errno.EINVAL, "Invalid parameters %s" % args)
 
-        if len(args) < 1:
-            raise VError(errno.EINVAL, "Insufficient parameters. %s" % args)
-
         if v_type == "connectivity":
+            if len(args) < 1:
+                raise VError(errno.EINVAL,
+                             "Insufficient parameters. %s" % args)
             self.validate_host_connectivity(args)
-        elif v_type == "passwordless":
-            self.validate_passwordless_ssh(args[0], args[1:])
+
         else:
-            raise VError(
-                errno.EINVAL, "Action parameter %s not supported" % v_type)
+            if len(args) < 2:
+                raise VError(errno.EINVAL,
+                             "Insufficient parameters. %s" % args)
+
+            if v_type == "passwordless":
+                self.validate_passwordless_ssh(args[0], args[1:])
+            elif v_type == "drivers":
+                self.validate_network_drivers(args[0], args[1:])
+            elif v_type == "hca":
+                # More providers can be added to the list in future, if required.
+                self.hca_checks = ['mellanox']
+                if args[0].lower() not in self.hca_checks:
+                    raise VError(errno.EINVAL,
+                                 "Invalid HCA Provider name. "
+                                 "Please choose from  %s" % self.hca_checks)
+                self.validate_hca(args[0], args[1:])
+            else:
+                raise VError(errno.EINVAL,
+                             "Action parameter %s not supported" % v_type)
 
     def validate_ip_connectivity(self, ips):
         """Check if IPs are reachable."""
@@ -92,7 +110,7 @@ class NetworkV:
 
     def _is_ip(self, ip):
         return (ip.count(".") == 3 and all(self._is_valid_ipv4_part(ip_part)
-           for ip_part in ip.split(".")))
+                for ip_part in ip.split(".")))
 
     def _is_valid_ipv4_part(self, ip_part):
         try:
@@ -106,3 +124,52 @@ class NetworkV:
         except Exception as exc:
             raise VError(errno.EINVAL,
                          f"Failed to resolve host {host} Error {str(exc)}")
+
+    def validate_network_drivers(self, driver, nodes):
+        """Check if drivers (eg: Mellanox OFED) are proper"""
+
+        for node in nodes:
+            cmd = f"ssh {node} rpm -qa | grep {driver}"
+            cmd_proc = SimpleProcess(cmd)
+            run_result = cmd_proc.run()
+
+            if run_result[1] or run_result[2]:
+                res = (f"Given Driver '{driver}' Is Not Installed on {node}. "
+                       f"Also, check if '{node}' is valid. "
+                       f"CMD {cmd} failed. {run_result[0]}. {run_result[1]}")
+                raise VError(errno.EINVAL, res)
+
+    def validate_hca(self, provider, nodes):
+        """Check if HCA presence and ports"""
+
+        for node in nodes:
+            if provider.lower() in self.hca_checks:
+                cmd = f"ssh {node} lspci -nn | grep '{provider.capitalize()}'"
+                cmd_proc = SimpleProcess(cmd)
+                run_result = cmd_proc.run()
+
+                if run_result[1] or run_result[2]:
+                    res = (f"{provider.capitalize()} Host Channel Adapters card "
+                           f"(possibly with Infiniband capability) is not detected on {node}. "
+                           f"Also, check if '{node}' is valid. "
+                           f"CMD '{cmd} failed. {run_result[0]}. {run_result[1]}")
+                    raise VError(errno.EINVAL, res)
+
+                ports_cmd = f"{cmd} | wc -l"
+                ports_cmd_proc = SimpleProcess(ports_cmd)
+                ports_run_result = ports_cmd_proc.run()
+
+                if ports_run_result[1] or ports_run_result[2]:
+                    res = (f"{provider.capitalize()} Host Channel Adapters ports were not "
+                           f"detected by command {cmd}. "
+                           f"Also, check if '{node}' is valid. "
+                           f"CMD {cmd} failed. {ports_run_result[0]}. {ports_run_result[1]}")
+                    raise VError(errno.EINVAL, res)
+
+                res = (ports_run_result[0].decode('utf-8').strip())
+                if int(res) == 0:
+                    res = (f"{provider.capitalize()} Host Channel Adapters ports were not "
+                           f"detected by command {cmd}. "
+                           "For high-speed data network, it is expected to "
+                           "have at least one port present over some PCIe slot. ")
+                    raise VError(errno.EINVAL, res)
