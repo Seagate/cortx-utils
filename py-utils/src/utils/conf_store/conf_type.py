5866af168e012b06caa0db2c860d20fdd4eaac2b
root <root@centos8.linuxvmimages.local>
2020-12-09 03:09:30 -0500

    confstore commit

---
+++ py-utils/src/utils/conf_store/conf_type.py
@@ -0,0 +1,6 @@
+from __future__ import absolute_import
+
+class ConfType():
+    def __init__(self, mode):
+        self.mode = mode
+
---
+++ py-utils/src/utils/conf_store/kvstorage_factory.py
@@ -0,0 +1,28 @@
+import os
+from src.utils.conf_store.json_storage import JsonStorage
+
+class KvStorageFactory:
+    """
+    Implements a KvStorage Factory.
+    """
+    def __init__(self, source):
+        self._source = source
+        self._MAP = {"json": JsonStorage, "yaml": JsonStorage}
+        self.__store = self.get_store_type()
+
+    def get_store_type(self):
+        try:
+            extension = os.path.splitext(self._source)[1][1:].strip().lower()
+            store_obj = self._MAP[extension]
+            return store_obj(self._source)
+        except KeyError as error:
+            raise KeyError(f"Unsupported file type:{error}")
+        except Exception as e:
+            raise Exception(f"Unable to read file {self._source}. {e}")
+
+    def load(self):
+        ''' Loads data from file of given format'''
+        return self.__store.load()
+
+    def dump(self, data):
+        pass
