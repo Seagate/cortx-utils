677fc53da8f2caaaa524bc5c3dbca82bc64e8abc
Ujjwal Lanjewar <ujjwal.lanjewar@seagate.com>
2020-12-10 14:55:37 +0530

    ConfStore Basic Implementation

--- py-utils/src/utils/conf_store/conf_cache.py
+++ py-utils/src/utils/conf_store/conf_cache.py
@@ -15,54 +15,89 @@
 # For any questions about this software or licensing,
 # please email opensource@seagate.com or cortx-questions@seagate.com.
 
+from cortx.utils.conf_store import ConfError
 
 class ConfCache:
-    """
-    ConfCache implements in-memory configuration management in specified format
-    """
+    """ In-memory configuration Data """
 
-    def __init__(self, kv_store_instance):
+    def __init__(self, kv_store: KvStore):
         self._dirty = False
-        self._kvstore = kv_store_instance
+        self._kv_store = kv_store
         self._data = {}
         self.load()
 
-    def load(self) -> dict:
-        if self._dirty:
-            raise Exception('%s not synced to disk' % self._kvstore)
-        self._data = self._kvstore.load()
+    @property
+    def data(self):
         return self._data
 
+    def load(self):
+        """ Loads the configuration from the KV backend """
+        if self._dirty:
+            raise Exception('%s not synced to disk' % self._kv_store)
+        self._data = self._kv_store.load()
+
+        self._keys = self.keys()
+
     def dump(self):
-        """
-        Dump the anifest file to desired file or to the source
-        """
-        self._kvstore.dump(self._data)
+        """ Dump the config values onto the corresponding KV backend """
+        self._kv_store.dump(self._data)
         self._dirty = False
 
-    def _get(self, key, data):
+    def _keys(self, data: dict, keys: list, pkey = None: str):
+        for key in data.keys():
+            if type(data[key]) == dict:
+                self._keys(data[key], keys, "{pkey}.{key}")
+            if type(data[key]) == str:
+                keys.append(key if pkey is None else "{pkey}.{key}")
+            else
+                raise ConfError(errno.ENOSYS, "Cant handle key %s", key)
+                
+    def keys(self):
+        """ Prepares the list of keys in the config cache """
+        self._keys = []
+        self._keys(self._data, self._keys)
+
+    def __iter__(self):
+        self._iter = 0
+        return self
+
+    def __next__(self):
+        if self_iter >= len(self._keys)
+            raise StopIteration
+        key = self._keys[self._iter]
+        self._iter += 1
+        return key
+
+    def _get(self, data: dict, key: str):
         """ Obtain value for the given key """
         k = key.split('.', 1)
         if k[0] not in data.keys(): return None
-        return self._get(k[1], data[k[0]]) if len(k) > 1 else data[k[0]]
+        return self._get(data[k[0]], k[1]) if len(k) > 1 else data[k[0]]
 
-    def get(self, key) -> dict:
-        if self._data is None:
-            raise Exception(f"Configuration {self._kvstore} not initialized")
+    def get(self, key: str):
+        """ Returns the value corresponding to the key """
         return self._get(key, self._data)
 
-    def _set(self, key, val, data):
+    def _set(self, data: dict, key: str, val):
         k = key.split('.', 1)
         if len(k) == 1:
             data[k[0]] = val
             return
-        if k[0] not in data.keys() or type(data[k[0]]) != self._kvstore._type:
-            data[k[0]] = {}
-        self._set(k[1], val, data[k[0]])
+        if k[0] not in data.keys(): data[k[0]] = {}
+        self._set(data[k[0]], k[1], val)
 
-    def set(self, key, val) -> None:
-        """
-        Sets the value into the DB for the given key
-        """
-        self._set(key, val, self._data)
+    def set(self, key: str, val):
+        """ Sets the value into the DB for the given key """
+        self._set(self._data, key, val)
         self._dirty = True
+        self._keys.append(key)
+
+    def _delete(self, data: dict, key: str):
+        k = key.split('.', 1)
+        if k[0] not in data.keys(): return 
+        if len(k) > 1: return self._delete(data[k[0]], k[1])
+        del data[k[0]]
+
+    def delete(self, key: str):
+        """ Delets a given key from the config """
+        self._delete(self._data, key)
--- py-utils/src/utils/conf_store/conf_store.py
+++ py-utils/src/utils/conf_store/conf_store.py
@@ -15,79 +15,95 @@
 # For any questions about this software or licensing,
 # please email opensource@seagate.com or cortx-questions@seagate.com.
 
-from cortx.utils.conf_store.conf_cache import ConfCache
-
+from cortx.utils.conf_store import ConfCache
+from cortx.utils.conf_store import ConfError
 
 class ConfStore:
-    """
-    ConfStore implements configuration management on top of KvStore
-    """
+    """ Configuration Store based on the KvStore """
+
     def __init__(self):
-        """
-        kvstore will be initialized at the time of load
-        """
-        self.kvstore = ''
-        self._payload = {}
+        """ kvstore will be initialized at the time of load """
+        self._cache = {}
 
-    def load(self, index, kvStore, force=False, callback=None) -> None:
+    def load(self, index: str, kvs_url: str, overwrite=False, callback=None):
         """
-        Args:
-            index (str): named key of the loaded value in the ConfCache
-            kvStore (object): instance of KvStorageFactory
-            force (bool): optional
-            callback (): W.I.P
-        """
-        if hasattr(kvStore, 'name'):
-            if (index in self._payload.keys() and force) or index not in self._payload.keys():
-                self._payload[index] = ConfCache(kvStore)
-            else:
-                raise Exception('index %s is already loaded')
-        else:
-            raise Exception('kvStore value should be instance of kvStoreFactory')
+        Loads the config from KV Store
 
-    def get(self, index, key=None, default_value=None) -> dict:
+        Parameters:
+        index:     Identifier for the config loaded from the KV Store
+        kv_store:  KV Store (Conf Backend)
+        overwrite: When False, it throws exception if index already exists. 
+                   Default: False 
+        callback:  Callback for the config changes in the KV Sto
         """
-        Args:
-            index (str): named key of the loaded value in the ConfCache
-            key (str): optional. key for the value to be retrieved.
-            key will be chained with '.' to denote nested structure.
-            eg. project.team.dev.name
-            default_value (dict) : optional. W.I.P
-        return:
-            Return type will be dict or string based of key
-        """
-        if key is not None:
-            return self._payload[index].get(key) if self._payload[index].get(key) is not None else default_value
-        else:
-            return self._payload[index].load() if self._payload[index] is not None else default_value
+        if index in self._cache.keys() and not overwrite: 
+            raise ConfError(errno.EINVAL, "conf index %s already exists", index)
 
-    def set(self, index, keyList, valueList):
-        #Todo: WIP
-        """
-        Args:
-            index (str): named key of the loaded value in the ConfCache
-            keyList (list): list of key's. Need to be iterated and stored.
-            valueList (list): list of value's, Need to be iterated and stored based of keylist.
-        """
-        pass
+        kv_store = KvStoreFactory.get_instance(kvs_url)
+        self._cache[index] = ConfCache(kv_store)
 
-    def copy(self):
-        # Todo: WIP
-        pass
-
-    def merge(self):
-        # Todo: WIP
-        pass
-
-    def save(self, index=None):
-        # Todo: WIP
+    def get(self, index: str, key: str, default_val=None):
         """
-        Args:
-            index (str): optional. named key to state which value need to be writen to the file.
-            If index is None then to save all the configurations.
-        """
-        pass
+        Paraeters:
+        index   Configuration Domain ID where config key values are stored
+        key     Configuration key. This can take two forms
+                1. "xyz" - Top Level Key
+                2. "x.y.z" - Key 'z' under x and y. Nested Structure.
+        default_val
+                Default Value
 
-    def backup(self):
-        # Todo: WIP
-        pass
+        Return Value:
+                Return type will be dict or string based of key
+        """
+        if index not in self._cache.keys():
+            raise ConfError(errno.EINVAL, "config index %s is not loaded", \
+                index)
+
+        val = self._cache[index].get(key)
+        if val is None:
+            return default_val
+
+    def set(self, index: str, key: str, val):
+        """
+        Obtain value for the given configuration 
+
+        Parameters:
+        index   Configuration Domain ID where config key values are stored
+        key     Configuration key. This can take two forms
+                1. "xyz" - Top Level Key
+                2. "x.y.z" - Key 'z' under x and y. Nested Structure.
+        val     Value to be set. Can be string or dict
+        """
+        if index not in self._cache.keys():
+            raise ConfError(errno.EINVAL, "config index %s is not loaded", \
+                index)
+
+        self._cache[index].set(key, val)
+
+    def copy(self, src_index: str, dst_index: str, overwrite = False: bool):
+        """
+        Copies one config domain to the other and saves
+        
+        Parameters:
+        src_index Source Index 
+        dst_index Destination Index 
+        """
+        if src_index not in self._cache.keys():
+            raise ConfError(errno.EINVAL, "config index %s is not loaded", \
+                src_index)
+
+        if dst_index not in self._cache.keys():
+            raise ConfError(errno.EINVAL, "config index %s is not loaded", \
+                dst_index)
+
+        i_src = iter(self._cache[src_index])
+        for key in next(i_src):
+            self._cache[dst_index].set(key, self._cache[src_index].get(key))
+        
+    def save(self, index: str):
+        """ Saves the given index configuration onto KV Store """
+        if index not in self._cache.keys():
+            raise ConfError(errno.EINVAL, "config index %s is not loaded", \
+                index)
+
+        self._cache[index].save()
