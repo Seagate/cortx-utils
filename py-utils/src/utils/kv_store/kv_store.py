6c05cc0ece30257b9a04c4e2318c0d0f62676260
Ujjwal Lanjewar <ujjwal.lanjewar@seagate.com>
2020-12-17 10:04:14 +0530

    KvStore Framework with Get Set Delete Interfaces (#129)
    * KV Store Basic Implementation
    
    * KvStore Framework with Get Set Delete Interfaces
    
    * KvStore Framework with Get Set Delete Interfaces
    
    * Kvstore feedback changes
    
    * Feedback changes
    
    * Feedback changes
    
    * added comments for kvstore package
    
    * KvStoreFactory method changes
    
    * KV Store Exception
    
    * New simplified architecture with testcases$
    
    * Updated delete patch
    
    * Comments & changes $
    
    * KvStore circular import fix and testcase updated
    
    * Removed kv_store_factory file
    
    * KvStore Framework with Get Set Delete Interfaces
    
    Co-authored-by: root <root@centos8.linuxvmimages.local>
    Co-authored-by: surya <k.suryakumar1024@gmail.com>

---
+++ py-utils/src/utils/kv_store/kv_store.py
@@ -0,0 +1,131 @@
+#!/bin/python3
+
+# CORTX Python common library.
+# Copyright (c) 2020 Seagate Technology LLC and/or its Affiliates
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+# For any questions about this software or licensing,
+# please email opensource@seagate.com or cortx-questions@seagate.com.
+
+import errno
+import inspect
+from urllib.parse import urlparse
+
+from cortx.utils.kv_store.error import KvStoreError
+
+
+class KvStore:
+    """ Abstraction over all kinds of KV based Storage """
+
+    def __init__(self, store_loc, store_path):
+        self._store_loc = store_loc
+        self._store_path = store_path
+
+    @property
+    def path(self):
+        return self._store_path
+
+    @property
+    def loc(self):
+        return self._store_loc
+
+    def _get(self, key: str, data: dict) -> str:
+        """ Obtain value for the given key """
+        k = key.split('.', 1)
+        if k[0] not in data.keys(): return None
+        return self._get(k[1], data[k[0]]) if len(k) > 1 else data[k[0]]
+
+    def get(self, keys: list) -> list:
+        """ Obtains values of keys. Return list of values. """
+        data = self.load()
+        vals = []
+        for key in keys:
+            val = self._get(key, data)
+            vals.append(val)
+        return vals
+
+    def _set(self, key: str, val: str, data: dict):
+        k = key.split('.', 1)
+        if len(k) == 1:
+            data[k[0]] = val
+            return
+        if k[0] not in data.keys():
+            data[k[0]] = {}
+        self._set(k[1], val, data[k[0]])
+
+    def set(self, keys: list, vals: list):
+        """ Updates a given set of keys and values """
+        if len(keys) != len(vals):
+            raise KvStoreError(errno.EINVAL, f"Mismatched keys & values %s:%s",\
+                keys, vals)
+        data = self.load()
+        for key, val in zip(keys, vals):
+            self._set(key, val, data)
+        self.dump(data)
+
+    def _delete(self, key: str, data: dict):
+        k = key.split('.', 1)
+        if len(k) == 1:
+            if k[0] in data.keys():
+                del data[k[0]]
+            return
+        if k[0] not in data.keys():
+            return
+        self._delete(k[1], data[k[0]])
+
+    def delete(self, keys: list):
+        """ Deletes given set of keys from the """
+        data = self.load()
+        for key in keys:
+            self._delete(key, data)
+        self.dump(data)
+
+    def load(self):
+        """ Loads and returns data from KV storage """
+        raise KvStoreError(errno.ENOSYS, f"%s:load() not implemented", \
+            type(self).__name__)
+
+    def dump(self, data):
+        """ Dumps data onto the KV Storage """
+        raise KvStoreError(errno.ENOSYS, f"%s:dump() not implemented", \
+            type(self).__name__)
+
+
+class KvStoreFactory:
+    """ Factory class for File based KV Store """
+
+    _stores = {}
+
+    def __init__(self):
+        """ Initializing KvStoreFactory"""
+        pass
+
+    @staticmethod
+    def get_instance(store_url: str) -> KvStore:
+        """ Obtain instance of KvStore for given file_type """
+
+        url_spec = urlparse(store_url)
+        store_type = url_spec.scheme
+        store_loc = url_spec.netloc
+        store_path = url_spec.path
+
+        if store_type in KvStoreFactory._stores.keys():
+            return KvStoreFactory._stores[store_type]
+
+        from cortx.utils.kv_store import kv_store_collection
+        storage = inspect.getmembers(kv_store_collection, inspect.isclass)
+        for name, cls in storage:
+            if hasattr(cls, 'name') and name != "KvStore" and store_type == cls.name:
+                KvStoreFactory._stores[store_type] = \
+                    cls(store_loc, store_path)
+                return KvStoreFactory._stores[store_type]
+
+        raise KvStoreError(errno.EINVAL, f"Invalid store type %s", store_type)
---
+++ py-utils/src/utils/kv_store/kv_store_collection.py
@@ -0,0 +1,195 @@
+#!/bin/python3
+
+# CORTX Python common library.
+# Copyright (c) 2020 Seagate Technology LLC and/or its Affiliates
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+# For any questions about this software or licensing,
+# please email opensource@seagate.com or cortx-questions@seagate.com.
+
+import json, toml, yaml, configparser
+from cortx.utils.process import SimpleProcess
+from cortx.utils.kv_store.kv_store import KvStore
+
+
+class JsonKvStore(KvStore):
+    """  Represents a JSON File Store """ 
+
+    name = "json"
+
+    def __init__(self, store_loc, store_path):
+        KvStore.__init__(self, store_loc, store_path)
+
+    def load(self) -> dict:
+        """ Reads from the file """
+        with open(self._store_path, 'r') as f:
+            return json.load(f)
+
+    def dump(self, data) -> None:
+        """ Saves data onto the file """
+        with open(self._store_path, 'w') as f:
+            json.dump(data, f, indent=2)
+
+
+class YamlKvStore(KvStore):
+    """  Represents a YAML File Store """ 
+
+    name = "yaml"
+
+    def __init__(self, store_loc, store_path):
+        KvStore.__init__(self, store_loc, store_path)
+
+    def load(self) -> dict:
+        """ Reads from the file """
+        with open(self._store_path, 'r') as f:
+            return yaml.safe_load(f)
+
+    def dump(self, data) -> None:
+        with open(self._store_path, 'w') as f:
+            yaml.dump(data, f)
+
+
+class TomlKvStore(KvStore):
+    """  Represents a TOML File Store """ 
+
+    name = "toml"
+
+    def __init__(self, store_loc, store_path):
+        KvStore.__init__(self, store_loc, store_path)
+
+    def load(self) -> dict:
+        """ Reads from the file """
+        with open(self._store_path, 'r') as f:
+            return toml.load(f, dict)
+
+    def dump(self, data) -> None:
+        """ Saves data onto the file """
+        with open(self._store_path, 'w') as f:
+            toml.dump(data, f)
+
+
+class IniKvStore(KvStore):
+    """  Represents a YAML File Store """ 
+
+    name = "ini"
+
+    def __init__(self, store_loc, store_path):
+        KvStore.__init__(self, store_loc, store_path)
+        self._config = configparser.ConfigParser()
+        self._type = configparser.SectionProxy
+
+    def load(self) -> dict:
+        """ Reads from the file """
+        self._config.read(self._store_path)
+        return self._config
+
+    def dump(self, data) -> None:
+        """ Saves data onto the file """
+        with open(self._store_path, 'w') as f:
+            data.write(f)
+
+
+class DictKvStore(KvStore):
+    """ Represents Dictionary Without file """ 
+
+    name = "dict"
+
+    def __init__(self, store_loc, store_path):
+        KvStore.__init__(self, store_loc, store_path)
+
+    def load(self) -> dict:
+        """ Reads from the file """
+        return self._store_path
+
+    def dump(self, data) -> None:
+        """ Saves data onto dictionary itself """
+        self._store_path = data
+
+
+class JsonMessageKvStore(JsonKvStore):
+    """ Represents and In Memory JSON Message """
+
+    name = "jsonmessage"
+
+    def __init__(self, store_loc, store_path):
+        """ 
+        Represents the Json Without FIle
+        :param json_str: Json String to be processed :type: str
+        """ 
+        JsonKvStore.__init__(self, store_loc, store_path)
+
+    def load(self) -> dict:
+        """ Load json to python Dictionary Object. Returns Dict """
+        return json.loads(self._store_path)
+
+    def dump(self, data: dict) -> None:
+        """ Sets data after converting to json """
+        self._store_path = json.dumps(data)
+
+
+class TextKvStore(KvStore):
+    """ Represents a TEXT File Store """ 
+
+    name = "text"
+
+    def __init__(self, store_loc, store_path):
+        KvStore.__init__(self, store_loc, store_path)
+
+    def load(self) -> dict:
+        """ Loads data from text file """ 
+        with open(self._store_path, 'r') as f:
+            return f.read()
+
+    def dump(self, data) -> None:
+        """ Dump the data to desired file or to the source """ 
+        with open(self._store_path, 'w') as f:
+            f.write(data)
+
+
+class PillarStore(KvStore):
+    """ Salt Pillar based KV Store """
+    name = "salt"
+
+    def __init__(self, store_loc, store_path):
+        KvStore.__init__(self, store_loc, store_path)
+
+    def get(self, key):
+        """Get pillar data for key."""
+        cmd = f"salt-call pillar.get {key} --out=json"
+        cmd_proc = SimpleProcess(cmd)
+        out, err, rc = cmd_proc.run()
+
+        if rc != 0:
+            if rc == 127: err = f"salt command not found"
+            raise KvError(rc, f"Cant get data for %s. %s.", key, err)
+
+        res = None
+        try:
+            res = json.loads(out)
+            res = res['local']
+
+        except Exception as ex:
+            raise KvError(errno.ENOENT, f"Cant get data for %s. %s.", \
+                key, ex)
+
+        if res is None:
+            raise KvError(errno.ENOENT, f"Cant get data for %s. %s." \
+                f"Key not present")
+
+        return res
+
+    def set(self, key, value):
+        # TODO: Implement
+        pass
+
+    def delete(self, key):
+        # TODO: Implement
+        pass
