be34a2878aace349563d25a591a06a7dc5dbc4cc
Shailesh Vadiya <shailesh.vaidya@seagate.com>
2021-01-05 10:34:48 +0530

    Opensource integrated CI (#135)
    * Added CentOS 7.8.2003 support
    
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>
    
    * Added DevOps steps
    
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>
    
    * Fixed build issues
    
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>
    
    * use generic docker-compose for all operating system
    
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>
    
    * added make help target
    
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>
    
    * reduce build time
    
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>
    
    * added statsd-utils package creation steps
    
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>
    
    * fixed syntax issue
    
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>

---
+++ docker/opensource-ci/set-githubrelease-repo.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -euf -o pipefail
+
+pip3 install githubrelease
+mkdir -p /cortx-build-dependencies && cd  /cortx-build-dependencies || exit
+export LC_ALL=en_US.utf8
+githubrelease asset Seagate/cortx download build-dependencies && /bin/createrepo -v . || exit
+yum-config-manager --add-repo file:///cortx-build-dependencies
+echo "gpgcheck=0" >> /etc/yum.repos.d/cortx-build-dependencies.repo
+yum clean all && rm -rf /var/cache/yum
+
