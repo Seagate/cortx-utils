0d024e00dc04665b71ee9fcf697c1da73cb2b40d
pratyush-seagate <pratyush.k.khan@seagate.com>
2021-01-20 08:36:43 -0700

    EOS-16423: S3 Mini-provisioner: Validations in post_install 	Change description: 		Add new validator APIs to check if services are running and if rpm or pip3 packages are installed on local or remote host 	Files edited/added: 		modified:   src/utils/validator/commands.py 		new file:   src/utils/validator/v_pkg.py 		new file:   src/utils/validator/v_service.py 		modified:   src/utils/validator/validate.py 		new file:   test/validator/test_pkg_validator.py 		new file:   test/validator/test_service_validator.py 	Testing done: 		Used newly created unit tests to verify the new APIs. used test_pkg_validator.py and test_service_validator.py 			> pip3 uninstall dist/cortx_py_utils-1.0.0-py3-none-any.whl -y;  rm -rf dist/* ;  python3 setup.py bdist_wheel && pip3 install dist/cortx_py_utils-1.0.0-py3-none-any.whl && python3  ./test/validator/test_pkg_validator.py  && python3 ./test/validator/test_service_validator.py
    			Successfully installed cortx-py-utils-1.0.0
    			.Password:
    			.Password:
    			Password:
    			..
    			----------------------------------------------------------------------
    			Ran 4 tests in 13.438s
    
    			OK
    			Password:
    			Password:
    			..
    			----------------------------------------------------------------------
    			Ran 2 tests in 4.679s
    
    			OK
    
    Signed-off-by: pratyush-seagate <pratyush.k.khan@seagate.com>

---
+++ py-utils/src/utils/validator/v_pkg.py
@@ -0,0 +1,71 @@
+#!/bin/env python3
+
+# CORTX Python common library.
+# Copyright (c) 2020 Seagate Technology LLC and/or its Affiliates
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+# For any questions about this software or licensing,
+# please email opensource@seagate.com or cortx-questions@seagate.com.
+
+import errno
+from cortx.utils.validator.error import VError
+from cortx.utils.process import SimpleProcess
+
+def search_pkg(cmd):
+	# print(f"Running {cmd}")
+	handler = SimpleProcess(cmd)
+	stdout, stderr, retcode = handler.run()
+	if retcode != 0:
+		raise VError(errno.EINVAL,
+			     "cmd: %s failed with error code: %d"
+			     %(cmd, retcode))
+	if stderr:
+		raise VError(errno.EINVAL,
+			     "cmd: %s failed with stderr: %s"
+			     %(cmd, stderr))
+
+class PkgV:
+	"""Pkg related validations."""
+
+	def validate(self, v_type: str, args: list, host: str = None):
+		"""
+		Process rpm validations.
+		Usage (arguments to be provided):
+		1. pkg validate_rpms host (optional) [packagenames]
+		2. pkg validate_pip3s host (optional) [pip3 packagenames]
+		"""
+
+		if v_type == "validate_rpms":
+			return self.validate_rpms(host, args)
+		elif v_type == "validate_pip3s":
+			return self.validate_pip3s(host, args)
+		else:
+			raise VError(errno.EINVAL,
+				     "Action parameter %s not supported" % v_type)
+
+	def validate_rpms(self, host, pkgs):
+		"""Check if rpm pkg is installed."""
+
+		for pkg in pkgs:
+			if host != None:
+				search_pkg(f"ssh {host} rpm -qa | grep {pkg}")
+			else:
+				search_pkg(f"rpm -qa | grep {pkg}")
+
+	def validate_pip3s(self, host, pkgs):
+		"""Check if pip3 pkg is installed."""
+
+		for pkg in pkgs:
+			if host != None:
+				search_pkg(f"ssh {host} pip3 list --format=legacy | grep {pkg}")
+			else:
+				search_pkg(f"pip3 list --format=legacy {pkg}")
---
+++ py-utils/src/utils/validator/v_service.py
@@ -0,0 +1,57 @@
+#!/bin/env python3
+
+# CORTX Python common library.
+# Copyright (c) 2020 Seagate Technology LLC and/or its Affiliates
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+# For any questions about this software or licensing,
+# please email opensource@seagate.com or cortx-questions@seagate.com.
+
+import errno
+from cortx.utils.validator.error import VError
+from cortx.utils.process import SimpleProcess
+
+class ServiceV:
+	"""Service related validations."""
+
+	def validate(self, v_type: str, args: list, host: str = None):
+		"""
+		Process service validations.
+		Usage (arguments to be provided):
+		1. service isrunning host (optional) [servicenames]
+		"""
+
+		if v_type == "isrunning":
+			return self.validate_services(host, args)
+		else:
+			raise VError(errno.EINVAL,
+				     "Action parameter %s not supported" % v_type)
+
+	def validate_services(self, host, services):
+		"""Check if services are running."""
+
+		for service in services:
+			if host != None:
+				cmd = f"ssh {host} systemctl status {service}"
+			else:
+				cmd = f"systemctl status {service}"
+			# print(f"Running {cmd}")
+			handler = SimpleProcess(cmd)
+			stdout, stderr, retcode = handler.run()
+			if retcode != 0:
+				raise VError(errno.EINVAL,
+					     "cmd: %s failed with error code: %d"
+					     %(cmd, retcode))
+			if stderr:
+				raise VError(errno.EINVAL,
+					     "cmd: %s failed with stderr: %s"
+					     %(cmd, stderr))
