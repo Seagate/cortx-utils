43e325a116beb2fc1ae99819772519098da4e4fd
Prabhjot <prabh.jot@seagate.com>
2021-01-28 01:00:34 -0700

    Added producer and consumer class

---
+++ py-utils/src/utils/IEM_framework/IEM_producer.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+# CORTX-Py-Utils: CORTX Python common library.
+# Copyright (c) 2020 Seagate Technology LLC and/or its Affiliates
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+# For any questions about this software or licensing,
+# please email opensource@seagate.com or cortx-questions@seagate.com.
+
+from  cortx.utils.IEM_framework import IEM
+
+class IEM_Producer:
+    cluster_id=None
+    site_id=None
+    rack_id=None    
+    node_id=None
+
+    def _init_(self,cluster_id,site_id,rack_id,node_id):
+        self.cluster_id=cluster_id
+        self.site_id=site_id
+        self.rack_id=rack_id
+        self..node_id=node_id
+
+    def send(self,iem:IEM):
+        return self.send(iem)
