57c8adb609f6c4709e19065ce971e17d5ce76015
Sachin Punadikar <sachin.punadikar@seagate.com>
2021-02-03 04:22:00 -0700

    Install python packages in post_install of utils_setup
    Currently, required python packages are getting installed as part of
    "%post" phase of rpm. This is incorrect way and need to handle the
    package dependency in "post_install" phase of miniprovisioner.
    
    Signed-off-by: Sachin Punadikar <sachin.punadikar@seagate.com>

---
+++ py-utils/src/utils/setup/msg_bus_test.py
@@ -0,0 +1,81 @@
+#!/bin/env python3
+
+# Copyright (c) 2021 Seagate Technology LLC and/or its Affiliates
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+# For any questions about this software or licensing,
+# please email opensource@seagate.com or cortx-questions@seagate.com.
+
+import errno
+import time
+from cortx.utils.message_bus import MessageBus, MessageConsumer, MessageProducer
+from cortx.utils.conf_store import Conf
+from cortx.utils.setup import SetupError
+from cortx.utils.process import SimpleProcess
+
+class MessageBusTest:
+    """ Represents Message Bus Test methods """
+
+    def __init__(self):
+        Conf.load("index", "json:///etc/cortx/message_bus.conf")
+        self._server = Conf.get("index",'message_broker>cluster[0]>server')
+        self._msg_bus = MessageBus()
+        # Create a test topic
+        cmd = "/opt/kafka/bin/kafka-topics.sh --create" + \
+              " --topic my_test --bootstrap-server " + self._server + ":9092"
+        try:
+            cmd_proc = SimpleProcess(cmd)
+            res_op, res_err, res_rc = cmd_proc.run()
+            if res_rc != 0:
+                raise SetupError(errno.EINVAL, "Unable to test the config")
+        except Exception as e:
+            raise SetupError(errno.EINVAL, \
+                             "Unable to test the config, %s", e)
+
+
+    def __del__(self):
+        # Delete the test topic
+        cmd = "/opt/kafka/bin/kafka-topics.sh --delete" + \
+              " --topic my_test --bootstrap-server " + self._server + ":9092"
+        try:
+            cmd_proc = SimpleProcess(cmd)
+            res_op, res_err, res_rc = cmd_proc.run()
+            if res_rc != 0:
+                raise SetupError(errno.EINVAL, "Unable to test the config")
+        except Exception as e:
+            raise SetupError(errno.EINVAL, \
+                             "Unable to test the config, %s", e)
+
+
+    def send_msg(self, message):
+        """ Sends a  message """
+        producer = MessageProducer(self._msg_bus, producer_id='setup', \
+            message_type='my_test', method='sync')
+        producer.send(message)
+        time.sleep(1)
+
+
+    def receive_msg(self):
+        """ Receives a message """
+        consumer = MessageConsumer(self._msg_bus, consumer_id='setup', \
+            consumer_group='provisioner', message_type=['my_test'], auto_ack=False, \
+            offset='earliest')
+        while True:
+            try:
+                time.sleep(1)
+                message = consumer.receive()
+                consumer.ack()
+                if message != None:
+                    return message
+            except Exception as e:
+                raise SetupError(errno.EINVAL, \
+                             "Unable to test the config, %s", e)
+
