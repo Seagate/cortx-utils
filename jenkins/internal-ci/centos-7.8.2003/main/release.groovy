a6f99b63125f8c489f445ccfbef19d98511a8692
Gowthaman Chinnathambi <56022993+gowthamchinna@users.noreply.github.com>
2021-02-09 10:07:33 +0530

    Main release issue (#230)
    Signed-off-by: Gowthaman Chinnathambi <gowthaman.chinnathambi@seagate.com>

--- jenkins/internal-ci/centos-7.8.2003/main/release.groovy
+++ jenkins/internal-ci/centos-7.8.2003/main/release.groovy
@@ -250,7 +250,7 @@ pipeline {
                 sh label: 'Tag last_successful', script: '''
                     pushd $integration_dir
                     test -L last_successful && rm -f last_successful
-                    test -L last_successful && rm -f last_successful_prod
+                    test -L last_successful_prod && rm -f last_successful_prod
                     ln -s $integration_dir/$release_tag/dev last_successful
                     ln -s $integration_dir/$release_tag/prod last_successful_prod
                     popd
