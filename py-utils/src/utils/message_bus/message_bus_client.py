9358dafe77a09fc6e907acca53e170c689230272
Selvakumar <selva.k.nambi@seagate.com>
2021-03-05 12:54:58 +0530

    Resolve overriding issue
    Signed-off-by: Selvakumar <selva.k.nambi@seagate.com>

--- py-utils/src/utils/message_bus/message_bus_client.py
+++ py-utils/src/utils/message_bus/message_bus_client.py
@@ -25,7 +25,7 @@ class MessageBusClient:
         **client_conf: dict):
         self._message_bus = message_bus
         self._message_bus.init_client(client_type, **client_conf)
-         self._client_conf = client_conf
+        self._client_conf = client_conf
 
     def _get_conf(self, key: str):
         """ To get the client configurations """
