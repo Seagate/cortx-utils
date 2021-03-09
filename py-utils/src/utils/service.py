fc3682c9a1060fd06c0cfa6578d6ce7129e4b42d
Shriya Deshmukh <shriya.deshmukh@seagate.com>
2021-03-09 09:35:15 -0700

    EOS-17636: Added function in service class to get service information.

--- py-utils/src/utils/service.py
+++ py-utils/src/utils/service.py
@@ -48,66 +48,117 @@ class ServiceHandler:
     def process(self, action, service_name):
         pass
 
-
-class DbusServiceHandler:
+class DbusServiceHandler(ServiceHandler):
     """ Handler for Service Control using DBUS interface """
     name = "dbus"
 
-    def get_systemd_interface(self):
+    def _get_systemd_interface():
         system_bus = dbus.SystemBus()
         systemd1 = system_bus.get_object('org.freedesktop.systemd1', '/org/freedesktop/systemd1')
         dbus_manager = dbus.Interface(systemd1, 'org.freedesktop.systemd1.Manager')
         return system_bus, dbus_manager
 
-    def process(self, action: str, service_name: str):
-        if action not in ['enable', 'disable', 'start', 'stop', 'restart']:
-            raise ServiceError(errno.EINVAL, "Invalid action '%s' for the service %s" \
-                %(action, service_name))
+    def start(self, service_name: str):
         try:
-            _, dbus_manager = self.get_systemd_interface()
-            if action == 'disable':
-                dbus_manager.DisableUnitFiles([f'{service_name}'], False)
-                dbus_manager.Reload()
-            elif action == 'enable':
-                dbus_manager.EnableUnitFiles([f'{service_name}'], False, True)
-                dbus_manager.Reload()
-            elif action == 'start':
-                dbus_manager.StartUnit(f'{service_name}', 'fail')
-            elif action == 'stop':
-                dbus_manager.StopUnit(f'{service_name}', 'fail')
-            elif action == 'restart':
-                dbus_manager.RestartUnit(f'{service_name}', 'fail')
-
+            _, dbus_manager = DbusServiceHandler._get_systemd_interface()
+            dbus_manager.StartUnit(f'{service_name}', 'fail')
         except dbus.DBusException as err:
-            raise ServiceError(errno.EINVAL, "Failed to '%s' on '%s' due to error. %s" \
-                %(action, service_name, err))
+            raise ServiceError(errno.EINVAL, "Failed to start %s due to error. %s" \
+                %(service_name, err))
 
-    def get_service_information(self, service_name):
-        """Returns service information: state, substate, pid, command_line_path."""
-        # Possible states:active, inactive, failed, deactivating, activating, reloading.
-        # Possible substates: failed, running, exited, stop-sigterm, stop-post, dead, start-pre.
+    def stop(self, service_name: str):
         try:
-            system_bus, dbus_manager = self.get_systemd_interface()
-            unit = system_bus.get_object('org.freedesktop.systemd1',dbus_manager.LoadUnit(service_name))
-            Iunit = dbus.Interface(unit, dbus_interface='org.freedesktop.DBus.Properties')
-            state = str(Iunit.Get('org.freedesktop.systemd1.Unit', 'ActiveState'))
-            substate = str(Iunit.Get('org.freedesktop.systemd1.Unit', 'SubState'))
-            pid = str(Iunit.Get('org.freedesktop.systemd1.Service', 'ExecMainPID'))
-            # Fetch command_line_path with argument(if any)
-            command_line =  list(Iunit.Get('org.freedesktop.systemd1.Service', 'ExecStart'))
-            return state, substate, pid, command_line
+            _, dbus_manager = DbusServiceHandler._get_systemd_interface()
+            dbus_manager.StopUnit(f'{service_name}', 'fail')
         except dbus.DBusException as err:
-            raise ServiceError(errno.EINVAL,"Can not fetch service information for %s"
-                                "service, due to error: %s." % (service_name, err))
+            raise ServiceError(errno.EINVAL, "Failed to stop %s due to error. %s"
+                %(service_name, err))
 
-    def check_service_is_enabled(self, service_name):
+    def restart(self, service_name: str):
+        try:
+            _, dbus_manager = DbusServiceHandler._get_systemd_interface()
+            dbus_manager.RestartUnit(f'{service_name}', 'fail')
+        except dbus.DBusException as err:
+            raise ServiceError(errno.EINVAL, "Failed to restart %s due to error. %s"
+                %(service_name, err))
+
+    def enable(self, service_name: str):
+        try:
+            _, dbus_manager = DbusServiceHandler._get_systemd_interface()
+            dbus_manager.EnableUnitFiles([f'{service_name}'], False, True)
+            dbus_manager.Reload()
+        except dbus.DBusException as err:
+            raise ServiceError(errno.EINVAL, "Failed to enable %s due to error. %s"
+                %(service_name, err))
+
+    def disable(self, service_name: str):
+        try:
+            _, dbus_manager = DbusServiceHandler._get_systemd_interface()
+            dbus_manager.DisableUnitFiles([f'{service_name}'], False)
+            dbus_manager.Reload()
+        except dbus.DBusException as err:
+            raise ServiceError(errno.EINVAL, "Failed to disable %s due to error. %s"
+                %(service_name, err))
+
+    def get_state(self, service_name):
+        """Returns ServiceState of the Service."""
+        system_bus, dbus_manager = DbusServiceHandler._get_systemd_interface()
+        unit = system_bus.get_object('org.freedesktop.systemd1',dbus_manager.LoadUnit(service_name))
+        Iunit = dbus.Interface(unit, dbus_interface='org.freedesktop.DBus.Properties')
+        pid = str(Iunit.Get('org.freedesktop.systemd1.Service', 'ExecMainPID'))
+        state = str(Iunit.Get('org.freedesktop.systemd1.Unit', 'ActiveState'))
+        substate = str(Iunit.Get('org.freedesktop.systemd1.Unit', 'SubState'))
+        command_line =  list(Iunit.Get('org.freedesktop.systemd1.Service', 'ExecStart'))
+        service_state = ServiceState()
+        service_state.pid = pid
+        service_state.state = state
+        service_state.substate = substate
+        service_state.command_line_path = command_line
+        return service_state
+
+    def is_enabled(self, service_name):
         """Returns service status: enable/disable."""
         try:
-            _, dbus_manager = self.get_systemd_interface()
+            _, dbus_manager = DbusServiceHandler._get_systemd_interface()
             return str(dbus_manager.GetUnitFileState(service_name))
         except dbus.DBusException as err:
-            raise ServiceError(errno.EINVAL,"Can not check service status: enable/disable for %s"
-                                "service, due to error: %s." % (service_name, err))
+            raise ServiceError(errno.EINVAL,"Can not check service status: enable/disable for %s, "
+                                "due to error: %s." % (service_name, err))
+
+class ServiceState:
+    """ Return service information: state, substate, pid, command_line_path. """
+
+    @property
+    def pid(self):
+        return self._pid
+
+    @property
+    def state(self):
+        return self._state
+
+    @property
+    def substate(self):
+        return self._substate
+
+    @property
+    def command_line_path(self):
+        return self._command_line_path
+
+    @pid.setter
+    def pid(self, pid):
+        self._pid = pid
+
+    @state.setter
+    def state(self, state):
+        self._state = state
+
+    @substate.setter
+    def substate(self, substate):
+        self._substate = substate
+
+    @command_line_path.setter
+    def command_line_path(self, command_line_path):
+        self._command_line_path = command_line_path
 
 class Service:
     """ Represents a Service which needs to be controlled """
@@ -115,17 +166,25 @@ class Service:
     def __init__(self, handler_type: str):
         self._handler = ServiceHandler.get(handler_type)
 
-    def process(self, action: str, *args):
-        self._handler.process(self, action, *args)
+    def start(self, service_name):
+        self._handler.start(self, service_name)
 
-    def get_systemd_interface(self):
-        systemd_bus, dbus_handler = self._handler.get_systemd_interface(self)
-        return systemd_bus, dbus_handler
+    def stop(self, service_name):
+        self._handler.stop(self, service_name)
 
-    def get_service_information(self, service_name):
-        state, substate, pid, path = self._handler.get_service_information(self, service_name)
-        return state, substate, pid, path
+    def restart(self, service_name):
+        self._handler.restart(self, service_name)
 
-    def check_service_is_enabled(self, service_name):
-        status = self._handler.check_service_is_enabled(self, service_name)
+    def enable(self, service_name):
+        self._handler.enable(self, service_name)
+
+    def disable(self, service_name):
+        self._handler.disable(self, service_name)
+
+    def get_state(self, service_name):
+        service_state = self._handler.get_state(self, service_name)
+        return service_state
+
+    def is_enabled(self, service_name):
+        status = self._handler.is_enabled(self, service_name)
         return status
