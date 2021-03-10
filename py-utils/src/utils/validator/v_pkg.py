1ef6a5f3920d6c6491787494b2eb4bd3d20479a5
MARIYAPPAN PONNUSAMY <mariyappan.ponnusamy@seagate.com>
2021-03-09 22:22:13 -0700

    Hanlde passwordless ssh not configured host
    Signed-off-by: MARIYAPPAN PONNUSAMY <mariyappan.ponnusamy@seagate.com>

--- py-utils/src/utils/validator/v_pkg.py
+++ py-utils/src/utils/validator/v_pkg.py
@@ -20,6 +20,8 @@ import re
 import os
 import pwd
 import errno
+import traceback
+from cortx.utils.ssh import SSHChannel
 from cortx.utils.validator.error import VError
 from cortx.utils.process import SimpleProcess
 from cortx.utils.validator.v_network import NetworkV
@@ -27,15 +29,29 @@ from cortx.utils.validator.v_network import NetworkV
 class PkgV:
     """Pkg related validations."""
 
-    def __search_pkg(self, cmd):
+    def __init__(self):
+        self.passwdless_ssh_enabled = False
+        self.ssh = None
+
+    def __execute_cmd(self, cmd):
+        """
+        Execute command using SSHChannel or SimpleProcees
+        Uses SSHChannel to execute ommand on host if passwordless ssh is not configured.
+        Uses SimpleProcess to execute command if no host is provided in args or
+        passwordless ssh is configured on host.
+        """
         # print(f"Running {cmd}")
-        handler = SimpleProcess(cmd)
-        stdout, stderr, retcode = handler.run()
+        if self.ssh:
+            retcode, result = self.ssh.execute(cmd)
+        else:
+            handler = SimpleProcess(cmd)
+            stdout, stderr, retcode = handler.run()
+            result = stdout.decode("utf-8") if retcode ==0 else stderr.decode("utf-8")
         if retcode != 0:
             raise VError(
                 errno.EINVAL,
-                "Package dependency check failed. cmd: %s stderr: %s" %(cmd, stderr))
-        return stdout.decode("utf-8")
+                "Command failure. cmd: %s stderr: %s" %(cmd, result))
+        return result
 
     def validate(self, v_type: str, args: list, host: str = None):
         """
@@ -44,11 +60,30 @@ class PkgV:
         1. pkg validate_rpms host (optional) [packagenames]
         2. pkg validate_pip3s host (optional) [pip3 packagenames]
         """
-
-        # Ensure we can perform passwordless ssh and there are no prompts
         if host:
-            NetworkV().validate('passwordless',
-                [pwd.getpwuid(os.getuid()).pw_name, host])
+            try:
+                # Ensure we can perform passwordless ssh and there are no prompts
+                NetworkV().validate('passwordless',
+                    [pwd.getpwuid(os.getuid()).pw_name, host])
+                self.passwdless_ssh_enabled = True
+            except VError:
+                # Create ssh connection in case passwordless ssh is not configured
+                host_details = re.search(r"(\w+):(.+)@(.+):(\d+)", host)
+                if not host_details or len(host_details.groups()) != 4:
+                    raise VError(
+                        errno.EINVAL,
+                        "Invalid host_url format is given for passwordless ssh not configured host.")
+                user = host_details.groups()[0]
+                passwd = host_details.groups()[1]
+                host = host_details.groups()[2]
+                port = host_details.groups()[3]
+                try:
+                    self.ssh = SSHChannel(
+                        host=host, username=user, password=passwd, port=port)
+                except:
+                    err = traceback.format_exc()
+                    raise VError(
+                        errno.EINVAL, "Failed to create ssh connection to %s, Error: %s" % (host, err))
 
         if v_type == "rpms":
             return self.validate_rpm_pkgs(host, args)
@@ -57,20 +92,26 @@ class PkgV:
         else:
             raise VError(errno.EINVAL, "Action parameter %s not supported" % v_type)
 
+        if self.ssh:
+            self.ssh.disconnect()
+
     def validate_rpm_pkgs(self, host, pkgs, skip_version_check=True):
         """Check if rpm pkg is installed."""
+        rpm_cmd = "rpm -qa"
 
         if not isinstance(pkgs, dict):
             skip_version_check = True
 
-        cmd = "ssh %s rpm -qa" % host if host != None else "rpm -qa"
-        result = self.__search_pkg(cmd)
+        if self.passwdless_ssh_enabled:
+            rpm_cmd = "ssh %s %s" % (host, rpm_cmd)
+
+        result = self.__execute_cmd(rpm_cmd)
 
         for pkg in pkgs:
-            if result.find(f"{pkg}") == -1:
+            if result.find("%s" % pkg) == -1:
                 raise VError(errno.EINVAL, "rpm pkg %s not installed." % pkg)
             if not skip_version_check:
-                matched_str = re.search(f"{pkg}-([^-][0-9.]+)-", result)
+                matched_str = re.search(r"%s-([^-][0-9.]+)-" % pkg, result)
                 installed_version = matched_str.groups()[0]
                 expected_version = pkgs[pkg]
                 if installed_version != expected_version:
@@ -79,18 +120,21 @@ class PkgV:
 
     def validate_pip3_pkgs(self, host, pkgs, skip_version_check=True):
         """Check if pip3 pkg is installed."""
+        pip3_cmd = "pip3 list"
 
         if not isinstance(pkgs, dict):
             skip_version_check = True
 
-        cmd = "ssh %s pip3 list" % host if host != None else "pip3 list"
-        result = self.__search_pkg(cmd)
+        if self.passwdless_ssh_enabled:
+            pip3_cmd = "ssh %s %s" % (host, pip3_cmd)
+
+        result = self.__execute_cmd(pip3_cmd)
 
         for pkg in pkgs:
-            if result.find(f"{pkg}") == -1:
+            if result.find("%s" % pkg) == -1:
                 raise VError(errno.EINVAL, "pip3 pkg %s not installed." % pkg)
             if not skip_version_check:
-                matched_str = re.search(f"{pkg} \((.*)\)", result)
+                matched_str = re.search(r"%s \((.*)\)" % pkg, result)
                 installed_version = matched_str.groups()[0]
                 expected_version = pkgs[pkg]
                 if installed_version != expected_version:
