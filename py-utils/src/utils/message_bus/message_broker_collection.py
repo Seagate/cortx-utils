9f0062ef2da471dc4680581e553acd798d5daa48
Selvakumar <selva.k.nambi@seagate.com>
2021-03-11 12:38:16 +0530

    Removing pandas for data processing of get_unread_count
    Signed-off-by: Selvakumar <selva.k.nambi@seagate.com>

--- py-utils/src/utils/message_bus/message_broker_collection.py
+++ py-utils/src/utils/message_bus/message_broker_collection.py
@@ -327,8 +327,8 @@ class KafkaMessageBroker(MessageBroker):
                 for each_row in rows:
                     new_row = [item for item in each_row if item != '']
                     table.append(new_row)
-                unread_count = [int(lag[5]) for lag in table if lag[5] != 'LAG' and \
-                    lag[5] != '-']
+                unread_count = [int(lag[5]) for lag in table if lag[5] != 'LAG' \
+                    and lag[5] != '-']
 
                 if len(unread_count) == 0:
                     raise MessageBusError(errno.EINVAL, "No active consumers \
