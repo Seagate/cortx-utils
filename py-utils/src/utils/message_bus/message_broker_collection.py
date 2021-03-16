39681c7eb7f3d3b0778cf50f33437b1f52377fd2
Selvakumar <selva.k.nambi@seagate.com>
2021-03-16 17:21:43 +0530

    Adding Testcases
    Signed-off-by: Selvakumar <selva.k.nambi@seagate.com>

--- py-utils/src/utils/message_bus/message_broker_collection.py
+++ py-utils/src/utils/message_bus/message_broker_collection.py
@@ -130,7 +130,7 @@ class KafkaMessageBroker(MessageBroker):
         Parameters:
         admin_id        A String that represents Admin client ID.
         """
-        m_type_partitions ={}
+        m_type_partitions = {}
         admin = self._clients['admin'][admin_id]
         message_types_list = self._get_metadata(admin)
         for m_type, partitions in message_types_list.items():
