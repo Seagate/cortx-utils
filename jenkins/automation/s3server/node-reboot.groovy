0247b0f4989c998babec76c4c0d149de66ca17c4
Shailesh Vaidya <shailesh.vaidya@seagate.com>
2021-03-23 11:01:18 +0530

    EOS-18568 Need to reboot jenkins VM in the pre-merge jenkins job for S3Server (#310)
    * Added s3server node reboot playbook
    
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>

---
+++ jenkins/automation/s3server/node-reboot.groovy
@@ -0,0 +1,71 @@
+pipeline {
+    agent { label 'docker-cp-centos-7.8.2003-node' }
+
+    options {
+        timeout(50)
+        timestamps()
+        ansiColor('xterm') 
+        disableConcurrentBuilds()
+        buildDiscarder(logRotator(numToKeepStr: "30"))
+    }
+
+    triggers { cron('30 21 * * *') }
+
+    environment {
+        VM_CRED = credentials('node-user')
+		JENKINS_API = credentials('jenkins_rest_token')
+    }
+
+    parameters {
+		string(name: 'REPO_URL', defaultValue: 'https://github.com/shailesh-vaidya/cortx-re', description: 'Repository URL to be used for cortx-re.')
+		string(name: 'REPO_BRANCH', defaultValue: 'main', description: 'Branch to be used for cortx-re repo.')
+		string(name: 'REBOOT_LABEL', defaultValue: 's3-dev-build-7.8.2003', description: 'Node Lable for reboot')
+	}
+
+    stages {
+        
+        stage ('Checkout Scripts') {
+            steps {
+                script {
+            	// Clone cortx-re repo
+                    dir('cortx-re') {
+						checkout([$class: 'GitSCM', branches: [[name: "$REPO_BRANCH"]], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'SubmoduleOption', disableSubmodules: false, parentCredentials: false, recursiveSubmodules: true, reference: '', trackingSubmodules: false]], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'cortx-admin-github', url: "$REPO_URL"]]])               
+                    }
+                }
+            }
+        }    
+
+		stage('Reboot Nodes') {
+            steps {
+                script {
+
+                // Move this code to Ansible playbook       
+                    def nodelist = nodesByLabel ("${params.REBOOT_LABEL}")
+                    print nodelist
+                    env.NODE_LIST=nodelist.join(",")
+
+                }	
+                
+                // Move this code to Ansible playbook
+                sh label: 'Install Ansible', returnStatus: true, script: """
+
+                yum install ansible -y
+                  
+                for i in \${NODE_LIST//,/ }; do NODE_IP_LIST=\$NODE_IP_LIST,\$(curl -s -X POST -L --user "$JENKINS_API_USR:$JENKINS_API_PSW" -d "script=println InetAddress.localHost.hostAddress" http://eos-jenkins.mero.colo.seagate.com/computer/\$i/scriptText); done
+                    
+                    pushd cortx-re/scripts/automation/server-reboot/
+                        #Take nodes offline
+                        ansible-playbook node-reboot.yml --tags jenkins-offline -i \$NODE_LIST  --extra-vars "ansible_ssh_pass=$VM_CRED_PSW jenkins_password="$JENKINS_API_PSW" jenkins_user=$JENKINS_API_USR"
+                        
+                        #Reboot nodes
+                        ansible-playbook node-reboot.yml --tags reboot -i \$NODE_IP_LIST  --extra-vars "ansible_ssh_pass=$VM_CRED_PSW"
+                        
+                        #Bring nodes online
+                        ansible-playbook node-reboot.yml --tags jenkins-online -i \$NODE_LIST  --extra-vars "ansible_ssh_pass=$VM_CRED_PSW jenkins_password="$JENKINS_API_PSW" jenkins_user=$JENKINS_API_USR"
+                    popd
+                    
+                """
+            }
+		}
+	}
+}
