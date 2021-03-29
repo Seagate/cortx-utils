1be3dd2448430beb976c2cf6934f8260f6dbbd31
suryakumar <suryakumar.kumaravelan@seagate.com>
2021-03-29 06:51:19 -0600

    Feedback changes
    Signed-off-by: suryakumar <suryakumar.kumaravelan@seagate.com>

--- py-utils/src/utils/kv_store/kv_payload.py
+++ py-utils/src/utils/kv_store/kv_payload.py
@@ -41,7 +41,7 @@ class KvPayload:
             return self._data
         return Format.dump(self._data, format_type)
 
-    def get_keys(self, **filters):
+    def get_keys(self, **filters) -> list:
         """
         Obtains list of keys stored in the payload
         Input Paramters:
@@ -184,7 +184,7 @@ class KvPayload:
         else:
             return self._delete(k[1], data[k[0]])
 
-    def delete(self, key):
+    def delete(self, key) -> bool:
         """
         Deletes given set of keys from the dictionary
         Return Value:
