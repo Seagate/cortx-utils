a39523fedbdf719766afe5bfa713acb3a8ce442c
Venkatesh K <venkatesh.k@seagate.com>
2021-03-30 10:08:46 +0530

    Added the script for syncing cortx and pune server builds (#316)
    * Added the script for syncing cortx and pune server builds
    
    Signed-off-by: Venkatesh K <venkatesh.k@seagate.com>
    
    * Updated the comments
    
    Signed-off-by: Venkatesh K <venkatesh.k@seagate.com>
    
    * Changing the script directory
    
    Signed-off-by: Venkatesh K <venkatesh.k@seagate.com>
    
    * Changing the script to env_setup directory
    
    Signed-off-by: Venkatesh K <venkatesh.k@seagate.com>
    
    * Addressing review comments
    
    Signed-off-by: Venkatesh K <venkatesh.k@seagate.com>
    
    * Addressing the review comments
    
    Signed-off-by: Venkatesh K <venkatesh.k@seagate.com>
    
    * Addressing the Codacy review comments 
    
    Signed-off-by: Venkatesh K <venkatesh.k@seagate.com>
    
    * Update sync_build.sh
    
    Signed-off-by: Venkatesh K <venkatesh.k@seagate.com>
    
    * Addressing the review comments
    
    Signed-off-by: Venkatesh K <venkatesh.k@seagate.com>
    
    * Addressing the Codacy review comments
    
    Signed-off-by: Venkatesh K <venkatesh.k@seagate.com>
    
    * Removed the commented line
    
    Signed-off-by: Venkatesh K <venkatesh.k@seagate.com>
    
    Co-authored-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>

---
+++ scripts/env_setup/sync_build.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+helpFunction()
+{
+   echo ""
+   echo "Usage: $0 -s source -d destination -c retention"
+   echo -e "\t-s Remote build path"
+   echo -e "\t-d Local build path"
+   echo -e "\t-c Specify number artifacts for the sync"
+   exit 1 # Exit script after printing help
+}
+
+while getopts "s:d:c:" opt
+do
+   case "$opt" in
+      s ) source="$OPTARG" ;;
+      d ) destination="$OPTARG" ;;
+      c ) count="$OPTARG" ;;
+      ? ) helpFunction ;; # Print helpFunction in case parameter is non-existent
+   esac
+done
+
+# Print helpFunction in case parameters are empty
+if [ -z "$source" ] || [ -z "$destination" ]
+then
+   echo "Some or all of the parameters are empty";
+   helpFunction
+fi
+
+if [ ! -d "$source" ]; then
+   echo "Error: Remote path '$source' is NOT found."
+   exit 1
+fi
+
+if [ ! -d "$destination" ]; then
+   echo "Warning: Local path '$destination' is NOT found."
+   echo "Creating the local path '$destination'..."
+   mkdir -p "$destination"
+fi
+
+pushd "$source"
+if [ -z "$count" ]; then
+   rsync -av --ignore-existing ./"$i" "$destination"
+   exit 1
+else
+   echo "searching for files to sync"
+   #only find last updated artifacts
+   folder_list=$(ls -1t | head -n "$count")
+   echo "Folders List: $folder_list"
+
+   for i in "${folder_list[@]}"
+   do
+     echo "syncing folder $i, this may take a while"
+     rsync -av --ignore-existing ./"$i" "$destination"
+   done
+fi
