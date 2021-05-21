e323ad8b8bda7f4141ba35d774a3773930619b71
Sachin Punadikar <sachin.punadikar@seagate.com>
2021-05-21 10:44:34 +0530

    Merge tmain into main (#302)
    * MessageBus: Rest Interface using aiohttp (#257)
    
    * EOS-20462: Adding rsyslog service restart. (#286)
    
    * Adding rsyslog service restart
    
    Signed-off-by: Udayan Yaragattikar <udayan.yaragattikar@seagate.com>
    
    * Codacy issues resolved
    
    Signed-off-by: Udayan Yaragattikar <udayan.yaragattikar@seagate.com>
    
    * Fix service handler
    
    Signed-off-by: Udayan Yaragattikar <udayan.yaragattikar@seagate.com>
    
    * reverting the change
    
    Signed-off-by: Udayan Yaragattikar <udayan.yaragattikar@seagate.com>
    
    * Resolve codacy issue
    
    * Resolving codacy issue
    
    Co-authored-by: Sachin Punadikar <sachin.punadikar@seagate.com>
    
    * MessageBus: Rest Interface using aiohttp
    
    This commit incorporated all the changes of PR 254.
    And a few modifications as well.
    
    Signed-off-by: Rahul Tripathi <rahul.tripathi@seagate.com>
    
    * Creating error class
    
    Signed-off-by: Rahul Tripathi <rahul.tripathi@seagate.com>
    
    * Revert "Creating error class"
    
    This reverts commit e7675f6a3add2c4d5935e08bb30812e373846bcd.
    
    * Fixes with some cosmetic changes
    
    Signed-off-by: Rahul Tripathi <rahul.tripathi@seagate.com>
    
    Co-authored-by: Udayan Yaragattikar <66424882+udayan-y21@users.noreply.github.com>
    Co-authored-by: Sachin Punadikar <sachin.punadikar@seagate.com>
    
    * EOS-16256: Instantiation of Event message and its attributes (#278)
    
    * Implementation of Iem set and print method
    
    Signed-off-by: Selvakumar <selva.k.nambi@seagate.com>
    
    * Change copyright year
    
    Signed-off-by: Selvakumar <selva.k.nambi@seagate.com>
    
    * Change in class interfaces
    
    Signed-off-by: Selvakumar <selva.k.nambi@seagate.com>
    
    * Cosmetic changes
    
    Signed-off-by: Selvakumar <selva.k.nambi@seagate.com>
    
    * Implemented send and receive
    
    Signed-off-by: Selvakumar <selva.k.nambi@seagate.com>
    
    * Change hex values to string and method parameters
    
    Signed-off-by: Selvakumar <selva.k.nambi@seagate.com>
    
    * Change hex values to string and method parameters
    
    Signed-off-by: Selvakumar <selva.k.nambi@seagate.com>
    
    * Initialise client in init by a bool parameter
    
    Signed-off-by: Selvakumar <selva.k.nambi@seagate.com>
    
    * Remove unneccesary validation
    
    Signed-off-by: Selvakumar <selva.k.nambi@seagate.com>
    
    * Added test code and Derived Error from UtilsError
    
    Signed-off-by: Selvakumar <selva.k.nambi@seagate.com>
    
    * Update Multiline comments
    
    Signed-off-by: Selvakumar <selva.k.nambi@seagate.com>
    
    * Defined Alert schema and added more testcases
    
    Signed-off-by: Selvakumar <selva.k.nambi@seagate.com>
    
    * Modify BaseError to UtilsError
    
    Signed-off-by: Selvakumar <selva.k.nambi@seagate.com>
    
    * fix codacy issues
    
    Signed-off-by: Selvakumar <selva.k.nambi@seagate.com>
    
    * fix codacy issues
    
    Signed-off-by: Selvakumar <selva.k.nambi@seagate.com>
    
    * change multiline comments
    
    Signed-off-by: Selvakumar <selva.k.nambi@seagate.com>
    
    * Make alert schema consistent
    
    Signed-off-by: Selvakumar <selva.k.nambi@seagate.com>
    
    * Resolve conflicting files
    
    Signed-off-by: Selvakumar <selva.k.nambi@seagate.com>
    
    Co-authored-by: rahul-tripathi-git <78896910+rahul-tripathi-git@users.noreply.github.com>
    Co-authored-by: Udayan Yaragattikar <66424882+udayan-y21@users.noreply.github.com>
    Co-authored-by: Selva Nambi <66383213+selvakumaar5496@users.noreply.github.com>

--- py-utils/src/utils/iem_framework/__init__.py
+++ py-utils/src/utils/iem_framework/__init__.py
@@ -1,5 +1,6 @@
-#!/bin/bash
-#
+#!/usr/bin/env python3
+
+# CORTX-Py-Utils: CORTX Python common library.
 # Copyright (c) 2021 Seagate Technology LLC and/or its Affiliates
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published
@@ -14,14 +15,7 @@
 # For any questions about this software or licensing,
 # please email opensource@seagate.com or cortx-questions@seagate.com.
 
-# Create /etc/cortx. This will be used for storing message_bus.conf file
-/bin/mkdir -p /etc/cortx
+__title__ = 'iem_framework'
 
-# Copy the setup_cli.py as utils_setup
-/bin/mkdir -p /opt/seagate/cortx/utils/bin
-/bin/cp -f /usr/lib/python3.6/site-packages/cortx/setup/utils_setup.py /opt/seagate/cortx/utils/bin/utils_setup
-/bin/chmod +x /opt/seagate/cortx/utils/bin/utils_setup
-
-# Copy the message_bus_server.py as message_bus_server
-/bin/cp -f /usr/lib/python3.6/site-packages/cortx/utils/message_bus/message_bus_server.py /opt/seagate/cortx/utils/bin/message_bus_server
-/bin/chmod +x /opt/seagate/cortx/utils/bin/message_bus_server
+from cortx.utils.iem_framework.event_message import EventMessage
+from cortx.utils.iem_framework.error import EventMessageError
--- py-utils/src/utils/rest_server/__init__.py
+++ py-utils/src/utils/rest_server/__init__.py
@@ -1,5 +1,6 @@
-#!/bin/bash
-#
+#!/usr/bin/env python3
+
+# CORTX-Py-Utils: CORTX Python common library.
 # Copyright (c) 2021 Seagate Technology LLC and/or its Affiliates
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published
@@ -14,14 +15,8 @@
 # For any questions about this software or licensing,
 # please email opensource@seagate.com or cortx-questions@seagate.com.
 
-# Create /etc/cortx. This will be used for storing message_bus.conf file
-/bin/mkdir -p /etc/cortx
+__title__ = 'rest_server'
 
-# Copy the setup_cli.py as utils_setup
-/bin/mkdir -p /opt/seagate/cortx/utils/bin
-/bin/cp -f /usr/lib/python3.6/site-packages/cortx/setup/utils_setup.py /opt/seagate/cortx/utils/bin/utils_setup
-/bin/chmod +x /opt/seagate/cortx/utils/bin/utils_setup
+from cortx.utils.rest_server.rest_server import RestServer
+from cortx.utils.rest_server.error import RestServerError
 
-# Copy the message_bus_server.py as message_bus_server
-/bin/cp -f /usr/lib/python3.6/site-packages/cortx/utils/message_bus/message_bus_server.py /opt/seagate/cortx/utils/bin/message_bus_server
-/bin/chmod +x /opt/seagate/cortx/utils/bin/message_bus_server
