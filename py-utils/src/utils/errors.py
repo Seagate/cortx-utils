23b62ba9c5c3824c5288d1bbb699318927c22e4e
Selva Nambi <66383213+selvakumaar5496@users.noreply.github.com>
2021-05-27 07:49:44 +0530

    EOS-16256: Instantiation of Event message and its attributes (#309)
    * Implementation of Iem set and print method
    
    Signed-off-by: Selvakumar <selva.k.nambi@seagate.com>
    
    * Change copyright year
    
    Signed-off-by: Selvakumar <selva.k.nambi@seagate.com>
    
    * Change in class interfaces
    
    Signed-off-by: Selvakumar <selva.k.nambi@seagate.com>
    
    * Cosmetic changes
    
    Signed-off-by: Selvakumar <selva.k.nambi@seagate.com>
    
    * Implemented send and receive
    
    Signed-off-by: Selvakumar <selva.k.nambi@seagate.com>
    
    * Change hex values to string and method parameters
    
    Signed-off-by: Selvakumar <selva.k.nambi@seagate.com>
    
    * Change hex values to string and method parameters
    
    Signed-off-by: Selvakumar <selva.k.nambi@seagate.com>
    
    * Initialise client in init by a bool parameter
    
    Signed-off-by: Selvakumar <selva.k.nambi@seagate.com>
    
    * Remove unneccesary validation
    
    Signed-off-by: Selvakumar <selva.k.nambi@seagate.com>
    
    * Added test code and Derived Error from UtilsError
    
    Signed-off-by: Selvakumar <selva.k.nambi@seagate.com>
    
    * Update Multiline comments
    
    Signed-off-by: Selvakumar <selva.k.nambi@seagate.com>
    
    * Defined Alert schema and added more testcases
    
    Signed-off-by: Selvakumar <selva.k.nambi@seagate.com>
    
    * Modify BaseError to UtilsError
    
    Signed-off-by: Selvakumar <selva.k.nambi@seagate.com>
    
    * fix codacy issues
    
    Signed-off-by: Selvakumar <selva.k.nambi@seagate.com>
    
    * fix codacy issues
    
    Signed-off-by: Selvakumar <selva.k.nambi@seagate.com>
    
    * change multiline comments
    
    Signed-off-by: Selvakumar <selva.k.nambi@seagate.com>
    
    * Make alert schema consistent
    
    Signed-off-by: Selvakumar <selva.k.nambi@seagate.com>
    
    * Resolve conflicting files
    
    Signed-off-by: Selvakumar <selva.k.nambi@seagate.com>
    
    * Adding BaseError
    
    Signed-off-by: Selvakumar <selva.k.nambi@seagate.com>
    
    Co-authored-by: Sachin Punadikar <sachin.punadikar@seagate.com>

--- py-utils/src/utils/errors.py
+++ py-utils/src/utils/errors.py
@@ -20,6 +20,27 @@ INTERNAL_ERROR = 0x1005
 ERR_OP_FAILED = 0x1100
 
 
+class UtilsError(Exception):
+    """ Generic Exception with error code and output """
+
+    def __init__(self, rc, message, *args):
+        self._rc = rc
+        self._desc = message % (args)
+
+    @property
+    def rc(self):
+        return self._rc
+
+    @property
+    def desc(self):
+        return self._desc
+
+    def __str__(self):
+        if self._rc == 0:
+            return self._desc
+        return "error(%d): %s" %(self._rc, self._desc)
+
+
 class BaseError(Exception):
     """ Parent class for the cli error classes """
 
