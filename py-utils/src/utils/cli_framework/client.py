48779833b7ccef49a1cac4ca674a34d2b55bb5d4
Udayan Yaragattikar <udayan.yaragattikar@seagate.com>
2021-06-08 05:42:38 +0000

    Addressing comments
    Signed-off-by: Udayan Yaragattikar <udayan.yaragattikar@seagate.com>

--- py-utils/src/utils/cli_framework/client.py
+++ py-utils/src/utils/cli_framework/client.py
@@ -1,5 +1,4 @@
 # CORTX-Py-Utils: CORTX Python common library.
-# CORTX-Py-Utils: CORTX Python common library.
 # Copyright (c) 2021 Seagate Technology LLC and/or its Affiliates
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published
