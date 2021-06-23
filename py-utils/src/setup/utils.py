5d16993877cbd9be681f7c359f305d4aaaf55b15
rohit-k-dwivedi <rohit.k.dwivedi@seagate.com>
2021-06-23 07:08:02 -0400

    Provided hooks for post/pre upgrade for both node and cluster level
    Signed-off-by: rohit-k-dwivedi <rohit.k.dwivedi@seagate.com>

--- py-utils/src/setup/utils.py
+++ py-utils/src/setup/utils.py
@@ -283,3 +283,15 @@ class Utils:
                     raise SetupError(e.errno, "Error deleting config file %s, \
                         %s", each_file, e)
         return 0
+
+    @staticmethod
+    def pre_upgrade(level: str):
+        """ pre upgrade hook for node level """
+        # TODO Perform corresponding actions for on node/cluster
+        return 0
+
+    @staticmethod
+    def post_upgrade(level: str):
+        """ post upgrade hook for node level """
+        # TODO Perform corresponding actions for on node/cluster
+        return 0
