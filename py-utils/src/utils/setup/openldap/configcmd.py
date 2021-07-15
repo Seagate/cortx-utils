5ba18b8f4edd4d02c2d2a1847f336edf9fc7354a
Shalaka Dharap <shalaka.dharap@seagate.com>
2021-07-14 23:18:39 -0600

    t Replication changes

--- py-utils/src/utils/setup/openldap/configcmd.py
+++ py-utils/src/utils/setup/openldap/configcmd.py
@@ -27,42 +27,32 @@ from  ast import literal_eval
 from setupcmd import SetupCmd, OpenldapPROVError
 from cortx.utils.process import SimpleProcess
 from base_configure_ldap import BaseConfig
+from setupReplication import Replication
+from pathlib import Path
 
 class ConfigCmd(SetupCmd):
   """Config Setup Cmd."""
   name = "config"
+  utils_tmp_dir = "/opt/seagate/cortx/utils/tmp"
 
   def __init__(self, config: str):
     """Constructor."""
     try:
       super(ConfigCmd, self).__init__(config)
-
       self.read_ldap_credentials()
-
     except Exception as e:
       raise OpenldapPROVError(f'exception: {e}\n')
 
   def process(self):
     """Main processing function."""
-    #sys.stdout.write(f"Processing {self.name} {self.url}\n")
-    #self.phase_prereqs_validate(self.name)
-    #self.phase_keys_validate(self.url, self.name)
-
     try:
       self.configure_openldap()
     except Exception as e:
       raise OpenldapPROVError(f'exception: {e}\n')
 
-
   def configure_openldap(self):
     """Install and Configure Openldap over Non-SSL."""
-    # 1. Install and Configure Openldap over Non-SSL.
-    # 2. Enable slapd logging in rsyslog config
-    # 3. Set openldap-replication
-    # 4. Check number of nodes in the cluster
     # Perform base configuration
-    #Put below inside a class  and method and then import it and call
-    #os.system('python3 ./../../third_party/openldap/base_configure_ldap.py --forceclean True --rootdnpasswd ' + self.rootdn_passwd)
     BaseConfig.performbaseconfig(self.rootdn_passwd.decode("utf-8"),'True')
     if os.path.isfile("/opt/seagate/cortx/s3/install/ldap/rsyslog.d/slapdlog.conf"):
       try:
@@ -72,7 +62,6 @@ class ConfigCmd(SetupCmd):
           raise OpenldapPROVError(f"mkdir /etc/rsyslog.d failed with errno: {e.errno}, exception: {e}\n")
       shutil.copy('/opt/seagate/cortx/s3/install/ldap/rsyslog.d/slapdlog.conf',
                   '/etc/rsyslog.d/slapdlog.conf')
-
     # restart rsyslog service
     try:
       sys.stdout.write("Restarting rsyslog service...\n")
@@ -81,18 +70,14 @@ class ConfigCmd(SetupCmd):
     except Exception as e:
       sys.stderr.write(f'Failed to restart rsyslog service, error: {e}\n')
       raise e
-    sys.stdout.write("Restarted rsyslog service...\n")
-
     # set openldap-replication
     self.configure_openldap_replication()
     
-    sys.stdout.write("INFO: Successfully configured openldap on the node.\n")
 
   def configure_openldap_replication(self):
     """Configure openldap replication within a storage set."""
     storage_set_count = self.get_confvalue(self.get_confkey(
         'CONFIG>CONFSTORE_STORAGE_SET_COUNT_KEY').replace("cluster-id", self.cluster_id))
-
     index = 0
     while index < int(storage_set_count):
       server_nodes_list = self.get_confkey(
@@ -101,26 +86,15 @@ class ConfigCmd(SetupCmd):
       if type(server_nodes_list) is str:
         # list is stored as string in the confstore file
         server_nodes_list = literal_eval(server_nodes_list)
-
       if len(server_nodes_list) > 1:
         sys.stdout.write(f'\nSetting ldap-replication for storage_set:{index}\n\n')
-
-        with open("hosts_list_file.txt", "w") as f:
+        Path(self.utils_tmp_dir).mkdir(parents=True, exist_ok=True)
+        ldap_hosts_list_file = os.path.join(self.utils_tmp_dir, "ldap_hosts_list_file.txt")
+        with open(ldap_hosts_list_file, "w") as f:
           for node_machine_id in server_nodes_list:
-            hostname = self.get_confvalue(f'server_node>{node_machine_id}>hostname')
-            f.write(f'{hostname}\n')
-        #TODO make python call below
-        #cmd = ['/opt/seagate/cortx/s3/install/ldap/replication/setupReplicationScript.sh',
-        #     '-h',
-        #     'hosts_list_file.txt',
-        #     '-p',
-        #     f'{self.rootdn_passwd}']
-        #handler = SimpleProcess(cmd)
-        #stdout, stderr, retcode = handler.run()
-
-        #os.remove("hosts_list_file.txt")
-
-        #if retcode != 0:
-        #  raise OpenldapPROVError(f"{cmd} failed with err: {stderr}, out: {stdout}, ret: {retcode}\n")
+            host_name = self.get_confvalue(f'server_node>{node_machine_id}>hostname')
+            f.write(f'{host_name}\n')
+        f.close()
+        Replication.setreplication(ldap_hosts_list_file,self.rootdn_passwd.decode("utf-8"))
+        os.remove(ldap_hosts_list_file)
       index += 1
-
