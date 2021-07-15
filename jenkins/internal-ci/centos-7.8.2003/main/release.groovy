58922665cc361c5a511963bfad008ab9f562a79d
Gaurav Chaudhari <gaurav.chaudhari@seagate.com>
2021-07-15 16:47:31 +0530

    Adhoc : Disable JIRA ticket creation for partition job (#513)
    * chore: disable jira creation for partition job
    
    Signed-off-by: Gaurav Chaudhari <gaurav.chaudhari@seagate.com>
    
    * chore: remove jira option for partition job
    
    Signed-off-by: Gaurav Chaudhari <gaurav.chaudhari@seagate.com>

--- jenkins/internal-ci/centos-7.8.2003/main/release.groovy
+++ jenkins/internal-ci/centos-7.8.2003/main/release.groovy
@@ -292,12 +292,10 @@ pipeline {
                         ]
                     build job: 'Partition Main Deploy 1N', propagate: false, wait: false, parameters: [
                             string(name: 'CORTX_BUILD', value: "http://cortx-storage.colo.seagate.com/releases/cortx/github/${branch}/${os_version}/${env.release_tag}/prod"),
-                            booleanParam(name: 'CREATE_JIRA_ISSUE_ON_FAILURE', value: true),
                             booleanParam(name: 'AUTOMATED', value: true)
                         ]
                     build job: 'Partition Main Deploy 3N', propagate: false, wait: false, parameters: [
                             string(name: 'CORTX_BUILD', value: "http://cortx-storage.colo.seagate.com/releases/cortx/github/${branch}/${os_version}/${env.release_tag}/prod"),
-                            booleanParam(name: 'CREATE_JIRA_ISSUE_ON_FAILURE', value: true),
                             booleanParam(name: 'AUTOMATED', value: true)
                         ]       
 				}
