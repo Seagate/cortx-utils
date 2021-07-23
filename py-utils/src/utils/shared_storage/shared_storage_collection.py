bc5163f1e507cd3258d7d812f42888b6c7e9c50e
Rahul Tripathi <rahul.tripathi@seagate.com>
2021-07-22 22:17:36 -0600

    EOS-22319: Shared Storage Framework
    Signed-off-by: Rahul Tripathi <rahul.tripathi@seagate.com>

---
+++ py-utils/src/utils/shared_storage/shared_storage_collection.py
@@ -0,0 +1,41 @@
+#!/bin/python3
+
+# CORTX Python common library.
+# Copyright (c) 2021 Seagate Technology LLC and/or its Affiliates
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+# For any questions about this software or licensing,
+# please email opensource@seagate.com or cortx-questions@seagate.com.
+
+import errno
+
+from cortx.utils import errors
+from cortx.utils.conf_store import Conf
+from cortx.utils.shared_storage import SharedStorageAgent
+from cortx.utils.shared_storage import SharedStorageError
+
+class GlusterSharedStorage(SharedStorageAgent):
+    """ GlusterFS based shared storage implementation """
+
+    name = "GlusterFS"
+
+    def __init__(self, shared_path: str  = ""):
+        self.shared_path = shared_path
+
+    def _fetch_path(self):
+        """ fetch path from confstore """
+        
+        shared_path = self.shared_path
+
+        if not shared_path:
+            raise SharedStorageError(errno.EINVAL, \
+                "shared_path not found in %s" % config_file_path)
+        return shared_path
