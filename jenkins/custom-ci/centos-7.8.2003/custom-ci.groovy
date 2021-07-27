9d20770f5512e7824b95555049193dab412ad54c
Shailesh Vaidya <shailesh.vaidya@seagate.com>
2021-07-27 17:35:49 +0530

    Reduced release build retention period (#530)
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>

--- jenkins/custom-ci/centos-7.8.2003/custom-ci.groovy
+++ jenkins/custom-ci/centos-7.8.2003/custom-ci.groovy
@@ -456,7 +456,7 @@ pipeline {
 		success {
 				sh label: 'Delete Old Builds', script: '''
 				set +x
-				find ${integration_dir}/* -maxdepth 0 -mtime +30 -type d -exec rm -rf {} \\;
+				find ${integration_dir}/* -maxdepth 0 -mtime +15 -type d -exec rm -rf {} \\;
 				'''
 		}
 	
