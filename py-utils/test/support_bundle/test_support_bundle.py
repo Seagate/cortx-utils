4d13f32801a7ade3e52f08b1bd4ebc8b73d5d9ee
Parayya Vastrad <56255712+vastradparayya@users.noreply.github.com>
2021-07-29 17:13:39 +0530

    EOS-22314: SupportBundle - NorthBound interfaces (#400)
    * Added support bundle north interfaces
    
    Signed-off-by: Parayya Vastrad <parayya.vastrad@seagate.com>
    
    * codacy issues
    
    Signed-off-by: Parayya Vastrad <parayya.vastrad@seagate.com>
    
    * Codacy issue
    
    Signed-off-by: Parayya Vastrad <parayya.vastrad@seagate.com>
    
    * Added testcases
    
    Signed-off-by: Parayya Vastrad <parayya.vastrad@seagate.com>
    
    * Coflict resolving
    
    Signed-off-by: Parayya Vastrad <parayya.vastrad@seagate.com>
    
    * Conflict
    
    Signed-off-by: Parayya Vastrad <parayya.vastrad@seagate.com>
    
    * Code consistency
    
    Signed-off-by: Parayya Vastrad <parayya.vastrad@seagate.com>
    
    * added setup
    
    Signed-off-by: Parayya Vastrad <parayya.vastrad@seagate.com>
    
    * Added testcase and code consistency
    
    Signed-off-by: Parayya Vastrad <parayya.vastrad@seagate.com>
    
    * Addressed comments
    
    Signed-off-by: Parayya Vastrad <parayya.vastrad@seagate.com>
    
    * Removed root user
    
    Signed-off-by: Parayya Vastrad <parayya.vastrad@seagate.com>
    
    * Worked on comments
    
    Signed-off-by: Parayya Vastrad <parayya.vastrad@seagate.com>
    
    * Codacy issue
    
    Signed-off-by: Parayya Vastrad <parayya.vastrad@seagate.com>
    
    * Added log.init in testcases
    
    Signed-off-by: Parayya Vastrad <parayya.vastrad@seagate.com>

---
+++ py-utils/test/support_bundle/test_support_bundle.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+
+# CORTX-Py-Utils: CORTX Python common library.
+# Copyright (c) 2021 Seagate Technology LLC and/or its Affiliates
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+# For any questions about this software or licensing,
+# please email opensource@seagate.com or cortx-questions@seagate.com.
+
+
+import os
+import unittest
+from cortx.utils.support_framework import SupportBundle
+from cortx.utils.support_framework import Bundle
+
+
+class TestSupportBundle(unittest.TestCase):
+
+    """Test Support Bundle related functionality."""
+    from cortx.utils.log import Log
+    from pathlib import Path
+    home = str(Path.home())
+    os.makedirs(f'{home}/test_sb', exist_ok=True)
+    Log.init('support_bundle', f'{home}/test_sb', level='DEBUG', \
+        backup_count=5, file_size_in_mb=5)
+    sb_description = "Test support bundle generation"
+    bundle_obj = SupportBundle.generate(comment=sb_description, components=['provisioner'])
+
+    def test_001generate(self):
+        self.assertIsNotNone(self.bundle_obj)
+        self.assertIsInstance(self.bundle_obj, Bundle)
+        self.assertIsInstance(self.bundle_obj.bundle_id, str)
+        self.assertIsInstance(self.bundle_obj.bundle_path, str)
+        self.assertNotEqual(self.bundle_obj.bundle_id, '')
+        self.assertNotEqual(self.bundle_obj.bundle_path, '')
+        self.assertEqual(self.bundle_obj.comment, self.sb_description)
+
+    def test_002generated_path(self):
+        from cortx.utils.conf_store import Conf
+        Conf.load('index', 'json:///etc/cortx/cluster.conf')
+        node_name = Conf.get('index', 'server_node>hostname')
+        tar_file_name = f"{self.bundle_obj.bundle_id}_{node_name}.tar.gz"
+        sb_file_path = f'/var/log/seagate/support_bundle/{tar_file_name}'
+        self.assertTrue(os.path.exists(sb_file_path.strip()))
+
+    def test_003status(self):
+        status = SupportBundle.get_status(bundle_id=self.bundle_obj.bundle_id)
+        self.assertNotEqual(status, {})
+        self.assertIsInstance(status, dict)
+
+
+if __name__ == '__main__':
+    unittest.main()
