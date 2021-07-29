d0cdfd1b1a9daef9de72e3733801d0a6fc63afe0
Parayya Vastrad <56255712+vastradparayya@users.noreply.github.com>
2021-07-29 17:13:39 +0530

    EOS-22314: SupportBundle - NorthBound interfaces (#400)
    * Added support bundle north interfaces
    
    Signed-off-by: Parayya Vastrad <parayya.vastrad@seagate.com>
    
    * codacy issues
    
    Signed-off-by: Parayya Vastrad <parayya.vastrad@seagate.com>
    
    * Codacy issue
    
    Signed-off-by: Parayya Vastrad <parayya.vastrad@seagate.com>
    
    * Added testcases
    
    Signed-off-by: Parayya Vastrad <parayya.vastrad@seagate.com>
    
    * Coflict resolving
    
    Signed-off-by: Parayya Vastrad <parayya.vastrad@seagate.com>
    
    * Conflict
    
    Signed-off-by: Parayya Vastrad <parayya.vastrad@seagate.com>
    
    * Code consistency
    
    Signed-off-by: Parayya Vastrad <parayya.vastrad@seagate.com>
    
    * added setup
    
    Signed-off-by: Parayya Vastrad <parayya.vastrad@seagate.com>
    
    * Added testcase and code consistency
    
    Signed-off-by: Parayya Vastrad <parayya.vastrad@seagate.com>
    
    * Addressed comments
    
    Signed-off-by: Parayya Vastrad <parayya.vastrad@seagate.com>
    
    * Removed root user
    
    Signed-off-by: Parayya Vastrad <parayya.vastrad@seagate.com>
    
    * Worked on comments
    
    Signed-off-by: Parayya Vastrad <parayya.vastrad@seagate.com>
    
    * Codacy issue
    
    Signed-off-by: Parayya Vastrad <parayya.vastrad@seagate.com>
    
    * Added log.init in testcases
    
    Signed-off-by: Parayya Vastrad <parayya.vastrad@seagate.com>

---
+++ py-utils/src/utils/support_framework/errors.py
@@ -0,0 +1,27 @@
+# CORTX-Py-Utils: CORTX Python common library.
+# Copyright (c) 2021 Seagate Technology LLC and/or its Affiliates
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+# For any questions about this software or licensing,
+# please email opensource@seagate.com or cortx-questions@seagate.com.
+from cortx.utils.errors import BaseError
+
+
+class BundleError(BaseError):
+
+    def __init__(self, rc=None, desc=None, message_id=None, message_args=None):
+        """Error class for support bundle related errors."""
+        super(BundleError, self).__init__(
+            rc, desc, message_id, message_args)
+
+    def __str__(self):
+        """Returns bundle_error in formatted way."""
+        return f"BundleError: {self._rc}: {self._desc}"
