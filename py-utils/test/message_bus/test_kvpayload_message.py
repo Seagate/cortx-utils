6bb9a7cebdbd475a1d20607ff654199519ebeca4
Selvakumar <selva.k.nambi@seagate.com>
2021-07-29 12:22:24 +0530

    Resolve codacy issue
    Signed-off-by: Selvakumar <selva.k.nambi@seagate.com>

--- py-utils/test/message_bus/test_kvpayload_message.py
+++ py-utils/test/message_bus/test_kvpayload_message.py
@@ -24,6 +24,7 @@ from cortx.utils.message_bus import MessageBus, MessageBusAdmin, \
 
 
 class TestKVPayloadMessage(unittest.TestCase):
+
     """Test Send/Receive KvPayload as message."""
 
     _message_type = 'kv_payloads'
