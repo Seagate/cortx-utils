c694af1eb5a1ec34bcb3f89edde057463358f08a
Parayya Vastrad <56255712+vastradparayya@users.noreply.github.com>
2021-07-30 20:52:18 +0530

    EOS-22130: Make use of support bundle code from utils to create support bundle (#384)
    * Added support bundle script which will invoke utils repo to generate upport bundle
    
    Signed-off-by: Parayya Vastrad <parayya.vastrad@seagate.com>
    
    * Added OO for support bundle script
    
    Signed-off-by: Parayya Vastrad <parayya.vastrad@seagate.com>
    
    * removed utils from support.yaml
    
    Signed-off-by: Parayya Vastrad <parayya.vastrad@seagate.com>
    
    * Moved support bundle script to scr/support dir
    
    Signed-off-by: Parayya Vastrad <parayya.vastrad@seagate.com>
    
    * Moved support dir to /src
    
    Signed-off-by: Parayya Vastrad <parayya.vastrad@seagate.com>
    
    * moved support bundle script to src/support
    
    Signed-off-by: Parayya Vastrad <parayya.vastrad@seagate.com>
    
    * Deleted unnecessary spaces
    
    Signed-off-by: Parayya Vastrad <parayya.vastrad@seagate.com>
    
    * Added srvnode-1
    
    Signed-off-by: Parayya Vastrad <parayya.vastrad@seagate.com>
    
    * Modified
    
    Signed-off-by: Parayya Vastrad <parayya.vastrad@seagate.com>
    
    * Bundle id manipulation
    
    Signed-off-by: Parayya Vastrad <parayya.vastrad@seagate.com>
    
    * Codacy issue
    
    Signed-off-by: Parayya Vastrad <parayya.vastrad@seagate.com>
    
    * Codacy issue
    
    Signed-off-by: Parayya Vastrad <parayya.vastrad@seagate.com>
    
    * Worked on comments
    
    Signed-off-by: Parayya Vastrad <parayya.vastrad@seagate.com>
    
    * renamed file
    
    Signed-off-by: Parayya Vastrad <parayya.vastrad@seagate.com>
    
    * added copyright
    
    Signed-off-by: Parayya Vastrad <parayya.vastrad@seagate.com>
    
    * added cli file to /bin
    
    Signed-off-by: Parayya Vastrad <parayya.vastrad@seagate.com>
    
    * Codacy issue
    
    Signed-off-by: Parayya Vastrad <parayya.vastrad@seagate.com>
    
    * Codacy issue
    
    Signed-off-by: Parayya Vastrad <parayya.vastrad@seagate.com>
    
    * changed support from support_framework
    
    Signed-off-by: Parayya Vastrad <parayya.vastrad@seagate.com>
    
    * support to support_framework
    
    Signed-off-by: Parayya Vastrad <parayya.vastrad@seagate.com>
    
    Co-authored-by: Sachin Punadikar <sachin.punadikar@seagate.com>

---
+++ py-utils/src/support/support_bundle_cli.py
@@ -0,0 +1,90 @@
+#!/bin/env python3
+
+# Copyright (c) 2021 Seagate Technology LLC and/or its Affiliates
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+# For any questions about this software or licensing,
+# please email opensource@seagate.com or cortx-questions@seagate.com.
+
+import asyncio
+import sys
+
+from cortx.utils.support_framework import SupportBundle
+from cortx.utils.cli_framework.command import Command
+
+
+class SupportBundleCli:
+    @staticmethod
+    def generate(comment: str, **kwargs):
+        components = ''
+        for key, value in kwargs.items():
+            if key == 'components':
+                components = value
+        options = {'comment': comment, 'components': components, 'comm': \
+            {'type': 'direct', 'target': 'utils.support_framework', 'method': \
+            'generate_bundle', 'class': 'SupportBundle', 'is_static': True, \
+            'params': {}, 'json': {}}, 'output': {}, 'need_confirmation': \
+            False, 'sub_command_name': 'generate_bundle'}
+
+        cmd_obj = Command('generate_bundle', options, [])
+        loop = asyncio.get_event_loop()
+        bundle_obj = loop.run_until_complete( \
+            SupportBundle._generate_bundle(cmd_obj))
+        return bundle_obj
+
+    @staticmethod
+    def get_status(bundle_id: str):
+         # Get the status of bundle
+        import time
+        time.sleep(5)
+
+        options = {'bundle_id': bundle_id, 'comm': {'type': 'direct', \
+            'target': 'utils.support_framework', 'method': 'get_bundle_status', \
+            'class': 'SupportBundle', \
+            'is_static': True, 'params': {}, 'json': {}},'output': {},\
+            'need_confirmation': False, 'sub_command_name': \
+            'get_bundle_status'}
+
+        cmd_obj = Command('get_bundle_status', options, [])
+        loop = asyncio.get_event_loop()
+        res = loop.run_until_complete(
+            SupportBundle._get_bundle_status(cmd_obj))
+        loop.close()
+        return res
+
+def main(argv):
+    from cortx.utils.conf_store.conf_store import Conf
+    from cortx.utils.log import Log
+
+    Conf.load('cortx_config', 'json:///etc/cortx/cortx.conf')
+    log_level = Conf.get('cortx_config', 'utils>log_level', 'INFO')
+    Log.init('support_bundle', '/var/log/cortx/utils/support', level=log_level, \
+        backup_count=5, file_size_in_mb=5)
+    
+    components = argv[3] if len(argv)>3 else []
+    if components:
+        components = [component for component in components.split(',')]
+    cmd = f"SupportBundleCli.{argv[1]}(comment='{argv[2]}', components={components})"
+    bundle_obj = eval(cmd)
+    print('bundle_id', bundle_obj.bundle_id)
+    status = SupportBundleCli.get_status(bundle_id=bundle_obj.bundle_id)
+    return status
+
+
+if __name__ == '__main__':
+    # componets parameter is optional, if not specified support bundle
+    # will be created for all components.
+    # Usage eg:
+    # $sudo /opt/seagate/cortx/utils/bin/support_bundle generate 'Creating support bundle generation'
+    # $sudo /opt/seagate/cortx/utils/bin/support_bundle generate 'Creating support bundle generation' 'utils,csm'
+    # support_bundle bin path is temporary and  will be replaced with support_bundle command
+    # while implementing support_bundle cli
+    sys.exit(main(sys.argv))
