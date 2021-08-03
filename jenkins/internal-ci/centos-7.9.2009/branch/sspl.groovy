a3baf4d5c0602bc7e7f07b2eab4b2241ef6b43ce
Shailesh Vaidya <shailesh.vaidya@seagate.com>
2021-08-03 10:39:07 +0530

    EOS-23219 SSPL Dev Sanity Test Job Gets Inappropriate Build Number (#533)
    * EOS-23219 SSPL Dev Sanity Test Job Gets Inappropriate Build Number
    
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>

--- jenkins/internal-ci/centos-7.9.2009/branch/sspl.groovy
+++ jenkins/internal-ci/centos-7.9.2009/branch/sspl.groovy
@@ -27,112 +27,109 @@ pipeline {
 
     stages {
 
-        stage('Checkout') {
-            steps {
-                script { build_stage = env.STAGE_NAME }
-                dir ('cortx-sspl') {
-                    checkout([$class: 'GitSCM', branches: [[name: "*/${branch}"]], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'AuthorInChangelog']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'cortx-admin-github', url: 'https://github.com/Seagate/cortx-monitor']]])
-                }
-            }
-        }
-        
-        stage('Build') {
-            steps {
-                script { build_stage = env.STAGE_NAME }
-                sh label: 'Build', returnStatus: true, script: '''
-                    set -xe
-                    pushd cortx-sspl
-                    VERSION=$(cat VERSION)
-                    export build_number=${BUILD_ID}
-                    #Execute build script
-                    echo "Executing build script"
-                    echo "VERSION:$VERSION"
-                    ./jenkins/build.sh -v $version -l DEBUG
-                    popd
-                '''    
-            }
-        }
-        
-        stage ('Upload') {
-            steps {
-                script { build_stage = env.STAGE_NAME }
-                sh label: 'Copy RPMS', script: '''
-                    mkdir -p $build_upload_dir/$BUILD_NUMBER
-                    cp /root/rpmbuild/RPMS/x86_64/*.rpm $build_upload_dir/$BUILD_NUMBER
-                    cp /root/rpmbuild/RPMS/noarch/*.rpm $build_upload_dir/$BUILD_NUMBER
-                '''
-                sh label: 'Repo Creation', script: '''pushd $build_upload_dir/$BUILD_NUMBER
-                    rpm -qi createrepo || yum install -y createrepo
-                    createrepo .
-                    popd
-                '''
-            }
-        }
-        
-        stage ('Tag last_successful') {
-            steps {
-                script { build_stage = env.STAGE_NAME }
-                sh label: 'Tag last_successful', script: '''
-                pushd $build_upload_dir/
-                    test -d $build_upload_dir/last_successful && rm -f last_successful
-                    ln -s $build_upload_dir/$BUILD_NUMBER last_successful
-                    popd
-                '''
-            }
-        }
-        stage ("Release") {
-            when { triggeredBy 'SCMTrigger' }
-            steps {
-                script { build_stage = env.STAGE_NAME }
-                script {
-                    def releaseBuild = build job: 'Release', propagate: true
-                     env.release_build = releaseBuild.number
-                    env.release_build_location="http://cortx-storage.colo.seagate.com/releases/cortx/github/$branch/$os_version/${env.release_build}"
-                }
-            }
-        } 
-        
-        stage ("Test") {
-            when { triggeredBy 'SCMTrigger' }
-            steps {
-                script { build_stage = env.STAGE_NAME }
-                script {
-                    build job: '../../SSPL/SSPL_Build_Sanity', propagate: false, wait: false,  parameters: [string(name: 'TARGET_BUILD', value: "main:${env.release_build}")]
-                }
-            }
-        }    
-    stage('Update Jira') {
-        when { expression { return env.release_build != null } }
-        steps {
-            script { build_stage=env.STAGE_NAME }
-                script {
-                    def jiraIssues = jiraIssueSelector(issueSelector: [$class: 'DefaultIssueSelector'])
-                    jiraIssues.each { issue ->
-                        def author =  getAuthor(issue)
-                        jiraAddComment(
-                            idOrKey: issue,
-                            site: "SEAGATE_JIRA",
-                            comment: "{panel:bgColor=#c1c7d0}"+
-                                "h2. ${component} - ${branch} branch build pipeline SUCCESS\n"+
-                                "h3. Build Info:  \n"+
-                                    author+
-                                        "* Component Build  :  ${BUILD_NUMBER} \n"+
-                                        "* Release Build    :  ${release_build}  \n\n  "+
-                                "h3. Artifact Location  :  \n"+
-                                    "*  "+"${release_build_location} "+"\n"+
-                                    "{panel}",
-                            failOnError: false,
-                            auditLog: false
-                        )
-                        //def jiraFileds = jiraGetIssue idOrKey: issue, site: "SEAGATE_JIRA", failOnError: false
-                        //if(jiraFileds.data != null){
-                        //def labels_data =  jiraFileds.data.fields.labels + "cortx_stable_b${release_build}"
-                         //jiraEditIssue idOrKey: issue, issue: [fields: [ labels: labels_data ]], site: "SEAGATE_JIRA", failOnError: false
-                        //}
+            stage('Checkout') {
+                steps {
+                    script { build_stage = env.STAGE_NAME }
+                    dir ('cortx-sspl') {
+                        checkout([$class: 'GitSCM', branches: [[name: "*/${branch}"]], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'AuthorInChangelog']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'cortx-admin-github', url: 'https://github.com/Seagate/cortx-monitor']]])
                     }
                 }
+            }
+            
+            stage('Build') {
+                steps {
+                    script { build_stage = env.STAGE_NAME }
+                    sh label: 'Build', returnStatus: true, script: '''
+                        set -xe
+                        pushd cortx-sspl
+                        VERSION=$(cat VERSION)
+                        export build_number=${BUILD_ID}
+                        #Execute build script
+                        echo "Executing build script"
+                        echo "VERSION:$VERSION"
+                        ./jenkins/build.sh -v $version -l DEBUG
+                        popd
+                    '''    
+                }
+            }
+            
+            stage ('Upload') {
+                steps {
+                    script { build_stage = env.STAGE_NAME }
+                    sh label: 'Copy RPMS', script: '''
+                        mkdir -p $build_upload_dir/$BUILD_NUMBER
+                        cp /root/rpmbuild/RPMS/x86_64/*.rpm $build_upload_dir/$BUILD_NUMBER
+                        cp /root/rpmbuild/RPMS/noarch/*.rpm $build_upload_dir/$BUILD_NUMBER
+                    '''
+                    sh label: 'Repo Creation', script: '''pushd $build_upload_dir/$BUILD_NUMBER
+                        rpm -qi createrepo || yum install -y createrepo
+                        createrepo .
+                        popd
+                    '''
+                }
+            }
+            
+            stage ('Tag last_successful') {
+                steps {
+                    script { build_stage = env.STAGE_NAME }
+                    sh label: 'Tag last_successful', script: '''
+                    pushd $build_upload_dir/
+                        test -d $build_upload_dir/last_successful && rm -f last_successful
+                        ln -s $build_upload_dir/$BUILD_NUMBER last_successful
+                        popd
+                    '''
+                }
+            }
+            
+            stage ('Release') {
+                when { triggeredBy 'SCMTrigger' }
+                steps {
+                    script { build_stage = env.STAGE_NAME }
+                    script {
+                        def releaseBuild = build job: 'Release', propagate: true
+                        env.release_build = releaseBuild.number
+                        env.release_build_location="http://cortx-storage.colo.seagate.com/releases/cortx/github/$branch/$os_version/${env.release_build}"
+                    }
+                }
+            }
+            
+            stage ('Test') {
+                when { triggeredBy 'SCMTrigger' }
+                steps {
+                    script { build_stage = env.STAGE_NAME }
+                    script {
+                        build job: "../../SSPL/SSPL_Build_Sanity_${os_version}", propagate: false, wait: false,  parameters: [string(name: 'TARGET_BUILD', value: "${branch}:${env.release_build}")]
+                    }
+                }
+            }
+            
+            stage('Update Jira') {
+                when { expression { return env.release_build != null } }
+                steps {
+                script { build_stage=env.STAGE_NAME }
+                script {
+                        def jiraIssues = jiraIssueSelector(issueSelector: [$class: 'DefaultIssueSelector'])
+                        jiraIssues.each { issue ->
+                            def author =  getAuthor(issue)
+                            jiraAddComment(
+                                idOrKey: issue,
+                                site: "SEAGATE_JIRA",
+                                comment: "{panel:bgColor=#c1c7d0}"+
+                                    "h2. ${component} - ${branch} branch build pipeline SUCCESS\n"+
+                                    "h3. Build Info:  \n"+
+                                        author+
+                                            "* Component Build  :  ${BUILD_NUMBER} \n"+
+                                            "* Release Build    :  ${release_build}  \n\n  "+
+                                    "h3. Artifact Location  :  \n"+
+                                        "*  "+"${release_build_location} "+"\n"+
+                                        "{panel}",
+                                failOnError: false,
+                                auditLog: false
+                            )
+                        }
+                    }
+            }
         }
-    }
     }
 
     post {
