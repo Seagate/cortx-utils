aa868efa82dc48755a5ebd6e9f9815888e085dd1
Rahul Tripathi <rahul.tripathi@seagate.com>
2021-08-06 00:40:59 -0600

    EOS-23272 Improvements on shared storage
    Signed-off-by: Rahul Tripathi <rahul.tripathi@seagate.com>

--- py-utils/src/utils/shared_storage/shared_storage_agent.py
+++ py-utils/src/utils/shared_storage/shared_storage_agent.py
@@ -14,6 +14,7 @@
 # please email opensource@seagate.com or cortx-questions@seagate.com.
 
 import inspect
+from urllib.parse import urlparse
 
 from cortx.utils import errors
 from cortx.utils.shared_storage import SharedStorageError
@@ -24,7 +25,8 @@ class SharedStorageAgent:
 
     def _fetch_path(self):
         """ fetch path from source """
-        pass
+        raise SharedStorageError(errno.EINVAL, \
+                "_fetch_path not implemented")
 
     def get_path(self):
         """ return fetched path of shared storage mountpoint """
@@ -39,8 +41,14 @@ class SharedStorageFactory:
     _storages = {}
 
     @staticmethod
-    def get_instance(storage_type: str, shared_path: str) -> SharedStorageAgent:
+    def get_instance(shared_storage_url: str) -> SharedStorageAgent:
         """ Obtain instance of SharedStorageAgent for given file_type """
+
+        url_spec = urlparse(shared_storage_url)
+        storage_type = url_spec.scheme
+        shared_loc = url_spec.netloc
+        shared_path = url_spec.path
+
         if storage_type in SharedStorageFactory._storages:
             return SharedStorageFactory._storages[storage_type]
 
