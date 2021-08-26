6940718331ec77b3b251ad9d4dab15e7b94a4cc9
shriya-deshmukh <shriya.deshmukh@seagate.com>
2021-08-26 13:50:43 +0530

    Fixed openldap import errors. (#483)
    Signed-off-by: Shriya Deshmukh <shriya.deshmukh@seagate.com>
    
    Co-authored-by: Sachin Punadikar <sachin.punadikar@seagate.com>

--- py-utils/src/utils/setup/openldap/cleanupcmd.py
+++ py-utils/src/utils/setup/openldap/cleanupcmd.py
@@ -21,9 +21,9 @@
 import os
 import ldap
 from cortx.utils.log import Log
-from setupcmd import SetupCmd, OpenldapPROVError
-from base_configure_ldap import BaseConfig
-from setupReplication import Replication
+from cortx.utils.setup.openldap.setupcmd import SetupCmd, OpenldapPROVError
+from cortx.utils.setup.openldap.base_configure_ldap import BaseConfig
+from cortx.utils.setup.openldap.setupReplication import Replication
 
 class CleanupCmd(SetupCmd):
     """Cleanup cmd initialization."""
