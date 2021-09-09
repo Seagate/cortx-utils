cdd0a089b5839f8b6c2a29813a65be67cba0f44a
Rahul Tripathi <78896910+rahul-tripathi-git@users.noreply.github.com>
2021-09-09 17:31:43 +0530

    Update build.sh

--- jenkins/build.sh
+++ jenkins/build.sh
@@ -19,7 +19,6 @@ PROG_NAME=$(basename "$0")
 BASE_DIR=$(realpath $(dirname "$0")/../py-utils)
 BUILD_NUMBER=
 GIT_VER=
-INSTALL_PATH=/opt/seagate
 
 #Following Install_path should be in sync with cortx.conf.sample config file.
 INSTALL_PATH=/opt/seagate
