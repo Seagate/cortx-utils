ee6d0dc5f9c5d0f1005832469e26d7b3f92c9d00
Veerendra Garikipati <veerendra.garikipati@seagate.com>
2021-09-12 20:02:47 +0000

    Adding SB unittests
    Signed-off-by: Veerendra Garikipati <veerendra.garikipati@seagate.com>

--- py-utils/test/support_bundle/test_support_bundle.py
+++ py-utils/test/support_bundle/test_support_bundle.py
@@ -23,104 +23,230 @@ import unittest
 from cortx.utils.conf_store import Conf
 from cortx.utils.support_framework import Bundle
 from cortx.utils.support_framework import SupportBundle
+from cortx.utils.process import SimpleProcess
+from cortx.utils.validator.v_service import ServiceV
 
 
 class TestSupportBundle(unittest.TestCase):
 
     """Test Support Bundle related functionality."""
-    from cortx.utils.log import Log
-    Log.init('support_bundle', '/var/log/cortx/utils/suppoort/', level='DEBUG', \
-        backup_count=5, file_size_in_mb=5)
-    sb_description = "Test support bundle generation"
 
-    def test_001generate(self):
-        bundle_obj = SupportBundle.generate(comment=self.sb_description, components=['provisioner'])
+    @classmethod
+    def setUpClass(cls):
+        """Test Setup class."""
+        from cortx.utils.log import Log
+        Log.init('support_bundle', '/var/log/cortx/utils/suppoort/', \
+            level='DEBUG', backup_count=5, file_size_in_mb=5)
+        cls.sb_description = "Test support bundle generation"
+        Conf.load('cluster_conf', 'json:///etc/cortx/cluster.conf')
+        cls.node_name = Conf.get('cluster_conf', 'cluster>srvnode-1')
+
+    def test_001_verify_SB_generate_single_comp(self):
+        """Validate SB generate for single component."""
+        bundle_obj = SupportBundle.generate(
+            comment=TestSupportBundle.sb_description, \
+                components=['provisioner'])
         self.assertIsNotNone(bundle_obj)
         self.assertIsInstance(bundle_obj, Bundle)
         self.assertIsInstance(bundle_obj.bundle_id, str)
         self.assertIsInstance(bundle_obj.bundle_path, str)
         self.assertNotEqual(bundle_obj.bundle_id, '')
         self.assertNotEqual(bundle_obj.bundle_path, '')
-        self.assertEqual(bundle_obj.comment, self.sb_description)
+        self.assertEqual(bundle_obj.comment, TestSupportBundle.sb_description)
         self.assertEqual(os.path.exists(f'{bundle_obj.bundle_path}'), True)
 
-    def test_002generated_path(self):
-        bundle_obj = SupportBundle.generate(comment=self.sb_description, components=['csm'])
-        time.sleep(10)
-        Conf.load('cluster_conf', 'json:///etc/cortx/cluster.conf')
-        node_name = Conf.get('cluster_conf', 'cluster>srvnode-1')
-        tar_file_name = f"{bundle_obj.bundle_id}_{node_name}.tar.gz"
-        sb_file_path = f'{bundle_obj.bundle_path}/{bundle_obj.bundle_id}/{node_name}/{tar_file_name}'
+    def test_002_verify_SB_generated_path(self):
+        """Validate SB generated path."""
+        bundle_obj = SupportBundle.generate(
+            comment=TestSupportBundle.sb_description, \
+                components=['provisioner'])
+        time.sleep(15)
+        tar_file_name = f"{bundle_obj.bundle_id}_"\
+            f"{TestSupportBundle.node_name}.tar.gz"
+        sb_file_path = f"{bundle_obj.bundle_path}/{bundle_obj.bundle_id}/"\
+            f"{TestSupportBundle.node_name}/{tar_file_name}"
         self.assertEqual(os.path.exists(sb_file_path), True)
 
-    def test_003status(self):
-        bundle_obj = SupportBundle.generate(comment=self.sb_description, components=['csm'])
-        status = SupportBundle.get_status(bundle_id=bundle_obj.bundle_id)
-        self.assertIsInstance(status, str)
-        self.assertIsInstance(json.loads(status), dict)
-
-    def test_004status(self):
-        bundle_obj = SupportBundle.generate(comment=self.sb_description, components=['csm'])
-        time.sleep(5)
+    def test_003_verify_SB_get_status_success_single_comp(self):
+        """Validate SB get_status success for single component."""
+        bundle_obj = SupportBundle.generate(
+            comment=TestSupportBundle.sb_description, components=['utils'])
+        time.sleep(15)
         status = SupportBundle.get_status(bundle_id=bundle_obj.bundle_id)
         self.assertIsNotNone(status)
+        self.assertIsInstance(status, str)
+        self.assertIsInstance(json.loads(status), dict)
         status = json.loads(status)
         if status['status']:
             self.assertEqual(status['status'][0]['result'], 'Success')
 
-    def test_005status(self):
-        bundle_obj = SupportBundle.generate(comment=self.sb_description, components=['wrong'])
+    def test_004_verify_SB_get_status_success_multi_comp(self):
+        """Validate SB get_status success for multiple components."""
+        bundle_obj = SupportBundle.generate(
+            comment=TestSupportBundle.sb_description, \
+                components=['csm', 'utils'])
+        time.sleep(30)
+        status = SupportBundle.get_status(bundle_id=bundle_obj.bundle_id)
+        self.assertIsNotNone(status)
+        self.assertIsInstance(status, str)
+        self.assertIsInstance(json.loads(status), dict)
+        status = json.loads(status)
+        if status['status']:
+            self.assertEqual(status['status'][0]['result'], 'Success')
+            self.assertEqual(status['status'][0]['result'], 'Success')
+
+    def test_005_verify_SB_get_status_empty_invalid_comp(self):
+        """Validate SB get_status empty for invalid component."""
+        # TODO - This test can be removed once bug(EOS-24882) is fixed
+        bundle_obj = SupportBundle.generate(
+            comment=TestSupportBundle.sb_description, components=['wrong'])
+        time.sleep(10)
         status = SupportBundle.get_status(bundle_id=bundle_obj.bundle_id)
         self.assertIsNotNone(status)
         status = json.loads(status)
         self.assertEqual(status['status'], [])
 
-    def test_006status(self):
+    def test_006_verify_SB_get_status_all_comp(self):
+        """Validate SB get_status for all component."""
         # TODO - Once all components are working this test case can be removed
         # Getting error because of some components dont have support.yaml and
         # empty support.yaml
-        bundle_obj = SupportBundle.generate(comment=self.sb_description)
+        bundle_obj = SupportBundle.generate(
+            comment=TestSupportBundle.sb_description)
+        time.sleep(20)
         status = SupportBundle.get_status(bundle_id=bundle_obj.bundle_id)
         status = json.loads(status)
         if status['status']:
             self.assertEqual(status['status'][0]['result'], 'Error')
 
-    def test_007_wrong_comp(self):
-        bundle_obj = SupportBundle.generate(comment=self.sb_description, components=['util'])
-        status = SupportBundle.get_status(bundle_id=bundle_obj.bundle_id)
-        status = json.loads(status)
-        if status['status']:
-            self.assertEqual(status['status'][0]['result'], 'Error')
-
-    def test_008_wrong_comp(self):
-        bundle_obj = SupportBundle.generate(comment=self.sb_description, components=['util;csmm'])
-        status = SupportBundle.get_status(bundle_id=bundle_obj.bundle_id)
-        status = json.loads(status)
-        if status['status']:
-            self.assertEqual(status['status'][0]['result'], 'Error')
-
-    def test_009_wrong_comp(self):
-        bundle_obj = SupportBundle.generate(comment=self.sb_description, components=['util;csm'])
+    def test_007_verify_SB_get_status_error_invalid_comp(self):
+        """Validate SB get_status error for invalid component."""
+        bundle_obj = SupportBundle.generate(
+            comment=TestSupportBundle.sb_description, components=['util'])
         time.sleep(10)
         status = SupportBundle.get_status(bundle_id=bundle_obj.bundle_id)
         status = json.loads(status)
         if status['status']:
             self.assertEqual(status['status'][0]['result'], 'Error')
 
-    def test_007_dir_remove(self):
-        bundle_obj = SupportBundle.generate(comment=self.sb_description, components=['csm'])
-        time.sleep(5)
-        Conf.load('cluster_conf', 'json:///etc/cortx/cluster.conf', skip_reload=True)
-        node_name = Conf.get('cluster_conf', 'cluster>srvnode-1')
-        self.assertFalse(os.path.exists(f'{bundle_obj.bundle_path}/{bundle_obj.bundle_id}/{node_name}/csm'))
-
-    def test_008_dir_remove(self):
-        bundle_obj = SupportBundle.generate(comment=self.sb_description, components=['csm', 'provisioner'])
+    def test_008_verify_SB_get_status_error_multi_invalid_comp(self):
+        """Validate SB get_status error for multiple invalid components."""
+        bundle_obj = SupportBundle.generate(
+            comment=TestSupportBundle.sb_description, \
+                components=['util', 'csmm'])
         time.sleep(15)
-        Conf.load('cluster_conf', 'json:///etc/cortx/cluster.conf', skip_reload=True)
-        node_name = Conf.get('cluster_conf', 'cluster>srvnode-1')
-        self.assertFalse(os.path.exists(f'{bundle_obj.bundle_path}/{bundle_obj.bundle_id}/{node_name}/csm'))
-        self.assertFalse(os.path.exists(f'{bundle_obj.bundle_path}/{bundle_obj.bundle_id}/{node_name}/provisioner'))
+        status = SupportBundle.get_status(bundle_id=bundle_obj.bundle_id)
+        status = json.loads(status)
+        if status['status']:
+            self.assertEqual(status['status'][0]['result'], 'Error')
+
+    def test_009_verify_SB_generate_comp_dir_remove(self):
+        """Validate component directories are removed after SB generate."""
+        bundle_obj = SupportBundle.generate(
+            comment=TestSupportBundle.sb_description, components=['csm'])
+        time.sleep(10)
+        self.assertFalse(os.path.exists(f'{bundle_obj.bundle_path}/\
+            {bundle_obj.bundle_id}/{TestSupportBundle.node_name}/csm'))
+
+    def test_010_verify_SB_generate_multi_comp_dir_remove(self):
+        """Validate multipe component directories are removed after SB generate."""
+        bundle_obj = SupportBundle.generate(
+            comment=TestSupportBundle.sb_description, \
+                components=['csm', 'provisioner'])
+        time.sleep(15)
+        self.assertFalse(os.path.exists(f'{bundle_obj.bundle_path}/\
+            {bundle_obj.bundle_id}/{TestSupportBundle.node_name}/csm'))
+        self.assertFalse(os.path.exists(f'{bundle_obj.bundle_path}/\
+            {bundle_obj.bundle_id}/{TestSupportBundle.node_name}/provisioner'))
+
+    def test_011_verify_SB_generate_after_rsyslog_service_stop(self):
+        """Validate SB generate while rsyslog service is down."""
+        cmd = "systemctl stop rsyslog"
+        cmd_proc = SimpleProcess(cmd)
+        _, _, rc = cmd_proc.run()
+        self.assertEqual(rc, 0)
+        bundle_obj = SupportBundle.generate(
+            comment=TestSupportBundle.sb_description, \
+                components=['provisioner'])
+        time.sleep(15)
+        tar_file_name = f"{bundle_obj.bundle_id}_"\
+            f"{TestSupportBundle.node_name}.tar.gz"
+        sb_file_path = f"{bundle_obj.bundle_path}/{bundle_obj.bundle_id}/"\
+            f"{TestSupportBundle.node_name}/{tar_file_name}"
+        self.assertEqual(os.path.exists(sb_file_path), True)
+        cmd = "systemctl start rsyslog"
+        cmd_proc = SimpleProcess(cmd)
+        _, _, rc = cmd_proc.run()
+        self.assertEqual(rc, 0)
+        ServiceV().validate('isrunning', ['rsyslog'])
+
+    def test_012_verify_SB_get_status_error_single_invalid_comp(self):
+        """Validate SB get_status error for single invalid component."""
+        bundle_obj = SupportBundle.generate(
+            comment=TestSupportBundle.sb_description, components=['util;csm'])
+        time.sleep(15)
+        status = SupportBundle.get_status(bundle_id=bundle_obj.bundle_id)
+        status = json.loads(status)
+        if status['status']:
+            self.assertEqual(status['status'][0]['result'], 'Error')
+
+    def test_013_verify_SB_generate_after_elasticsearch_service_stop(self):
+        """Validate SB generate while elasticsearch service is down."""
+        cmd = "systemctl stop elasticsearch"
+        cmd_proc = SimpleProcess(cmd)
+        _, _, rc = cmd_proc.run()
+        self.assertEqual(rc, 0)
+        bundle_obj = SupportBundle.generate(
+            comment=TestSupportBundle.sb_description, \
+                components=['provisioner'])
+        time.sleep(15)
+        tar_file_name = f"{bundle_obj.bundle_id}_"\
+            f"{TestSupportBundle.node_name}.tar.gz"
+        sb_file_path = f"{bundle_obj.bundle_path}/{bundle_obj.bundle_id}/"\
+            f"{TestSupportBundle.node_name}/{tar_file_name}"
+        self.assertEqual(os.path.exists(sb_file_path), True)
+        cmd = "systemctl start elasticsearch"
+        cmd_proc = SimpleProcess(cmd)
+        _, _, rc = cmd_proc.run()
+        self.assertEqual(rc, 0)
+        ServiceV().validate('isrunning', ['elasticsearch'])
+
+    def test_014_verify_SB_get_status_after_cluster_stop(self):
+        """Validate SB get_status while cluster is down."""
+        cmd = "pcs cluster stop --all"
+        cmd_proc = SimpleProcess(cmd)
+        _, _, rc = cmd_proc.run()
+        self.assertEqual(rc, 0)
+        bundle_obj = SupportBundle.generate(
+            comment=TestSupportBundle.sb_description, components=['csm'])
+        time.sleep(15)
+        tar_file_name = f"{bundle_obj.bundle_id}_"\
+            f"{TestSupportBundle.node_name}.tar.gz"
+        sb_file_path = f"{bundle_obj.bundle_path}/{bundle_obj.bundle_id}/"\
+            f"{TestSupportBundle.node_name}/{tar_file_name}"
+        self.assertEqual(os.path.exists(sb_file_path), True)
+        status = SupportBundle.get_status(bundle_id=bundle_obj.bundle_id)
+        self.assertIsNotNone(status)
+        self.assertIsInstance(status, str)
+        print(status)
+        status = json.loads(status)
+        if status['status']:
+            self.assertEqual(status['status'][0]['result'], 'Success')
+            cmd = "pcs cluster start --all"
+        cmd_proc = SimpleProcess(cmd)
+        _, _, rc = cmd_proc.run()
+        self.assertEqual(rc, 0)
+        time.sleep(5)
+
+    @classmethod
+    def tearDownClass(cls):
+        """Test teardown class."""
+        cmds = ["systemctl start rsyslog", "systemctl start elasticsearch", \
+            "pcs cluster start --all"]
+        for cmd in cmds:
+            cmd_proc = SimpleProcess(cmd)
+            _, _, rc = cmd_proc.run()
+            assert(rc == 0)
 
 
 if __name__ == '__main__':
