51c5a3c2ff14df40de2cef1208384ad89d6d15ad
Parayya Vastrad <parayya.vastrad@seagate.com>
2021-09-16 13:29:39 +0000

    Log
    Signed-off-by: Parayya Vastrad <parayya.vastrad@seagate.com>

--- py-utils/src/utils/support_framework/bundle_generate.py
+++ py-utils/src/utils/support_framework/bundle_generate.py
@@ -115,8 +115,7 @@ class ComponentsBundle:
         Conf.load('cortx_conf', 'json:///etc/cortx/cortx.conf')
         log_level = Conf.get('cortx_conf', 'utils>log_level', 'INFO')
         Log.init('support_bundle_node', '/var/log/cortx/utils/support/', \
-            level=log_level, backup_count=5, file_size_in_mb=5, \
-            syslog_server='localhost', syslog_port=514)
+            level=log_level, backup_count=5, file_size_in_mb=5)
         bundle_id = command.options.get(const.SB_BUNDLE_ID, '')
         node_name = command.options.get(const.SB_NODE_NAME, '')
         comment = command.options.get(const.SB_COMMENT, '')
@@ -220,6 +219,10 @@ class ComponentsBundle:
 
         ComponentsBundle._create_summary_file(bundle_id, node_name, \
             comment, bundle_path)
+        # SB status logging
+        Log.init('support_bundle_status', '/var/log/cortx/utils/support/', \
+            level=log_level, backup_count=5, file_size_in_mb=5, \
+            syslog_server='localhost', syslog_port=514)
         # Wait Until all the Threads Execution is not Complete.
         for each_thread in threads:
             Log.debug(
