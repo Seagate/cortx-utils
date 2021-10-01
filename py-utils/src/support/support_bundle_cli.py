16d6634c6345cac608d7fb5062a1f640059cd06a
Palak Garg <palak.garg@seagate.com>
2021-10-01 08:41:35 -0600

    EOS-24420: Create CORTX support bundle in K8 deployment
    Signed-off-by: Palak Garg <palak.garg@seagate.com>

--- py-utils/src/support/support_bundle_cli.py
+++ py-utils/src/support/support_bundle_cli.py
@@ -39,8 +39,15 @@ class SupportBundleCli:
                 Support Bundle!")
         comment = args.comment[0]
         components = args.component[0].split(';') if args.component else []
+        path = args.URL[0]
+        if "http://" in path:
+            sys.stderr.write("URL format target is not implemented yet.\n"
+                "Please specify the absolute target path.\n"
+                "For example:-\n"
+                "support_bundle generate 'test cortx' -t '/var/cortx/support_bundle'.\n")
+            sys.exit(1)
         bundle_obj = SupportBundle.generate(comment=comment, \
-            components=components)
+            components=components, target_path=path)
         display_string_len = len(bundle_obj.bundle_id) + 4
         response_msg = (
             f"Please use the below bundle id for checking the status of support bundle."
@@ -79,6 +86,8 @@ class GenerateCmd:
         s_parser.add_argument('comment', nargs='+', help='Comment - Reason for generating Support Bundle')
         s_parser.add_argument('-c', '--component', nargs='+', default=[], \
             help='Optional, Specified component support bundle will be generated')
+        s_parser.add_argument('-t', '--URL', nargs='+', required=True, \
+            help='Path- CORTX support bundle will be generated at specified path.')
 
 
 class StatusCmd:
