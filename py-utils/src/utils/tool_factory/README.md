4055c30e5f7cb7d527aa922b6f8dbaf8c28c6d91
Palak Garg <palak.garg@seagate.com>
2021-10-21 11:02:53 -0600

    - Implemented SB Extractor shell script. - Added README.md
    Signed-off-by: Palak Garg <palak.garg@seagate.com>

---
+++ py-utils/src/utils/tool_factory/README.md
@@ -0,0 +1,37 @@
+<!--                                                                            
+Tool Factory: CORTX Python common tool library.                                    
+Copyright (c) 2021 Seagate Technology LLC and/or its Affiliates                 
+This program is free software: you can redistribute it and/or modify            
+it under the terms of the GNU Affero General Public License as published        
+by the Free Software Foundation, either version 3 of the License, or            
+(at your option) any later version.                                             
+This program is distributed in the hope that it will be useful,                 
+but WITHOUT ANY WARRANTY; without even the implied warranty of                  
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                    
+GNU Affero General Public License for more details.                             
+You should have received a copy of the GNU Affero General Public License        
+along with this program. If not, see <https://www.gnu.org/licenses/>.           
+For any questions about this software or licensing,                             
+please email opensource@seagate.com or cortx-questions@seagate.com.          
+-->
+
+# Support Bundle Extractor Utility
+
+---
+
+## Clone
+
+```bash
+git clone https://github.com/Seagate/cortx-utils.git
+```
+
+### Procedure to extract SB
+
+*   Execute extract_support_bundle.sh script from entrypoint passing args:
+<file_path of tar archive> <destination> [Optional Parameter, Default: $PWD]
+<components_list as a string seperated by space> [Optional Parameter, Default: "All components"]
+```bash
+<install_path>/py-utils/src/utils/tool_factory/extract_support_bundle.sh
+-f '/tmp/SB09995_8d00f71d6e0eeef02e971e6356bb83aa.tar.gz'
+-d '/var/cortx/support_bundle' -c "common utils"
+```
---
+++ py-utils/src/utils/tool_factory/extract_support_bundle.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+#
+# Copyright (c) 2021 Seagate Technology LLC and/or its Affiliates
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+# For any questions about this software or licensing,
+# please email opensource@seagate.com or cortx-questions@seagate.com.
+
+PROG_NAME=$(basename "$0")
+TARFILE=
+
+usage() {
+    echo """usage: $PROG_NAME [-f file_path] [-t target_path] [-c components]""" 1>&2;
+    exit 1;
+}
+
+# Check for passed in arguments
+while getopts ":f:d:c:" opt; do
+    case "${opt}" in
+        f)
+            TARFILE=${OPTARG}
+            ;;
+        d)
+            DEST=${OPTARG}
+            ;;
+        c)
+            COMPONENTS=${OPTARG}
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+
+if [ -z $"$TARFILE" ];
+then
+    echo "Required TARFILE path is missing/invalid."
+    exit 1
+fi
+[ -z "$DEST" ] && DEST="$(pwd)"
+[ -z "$COMPONENTS" ] && COMPONENTS="all"
+
+# Extract the Support Bundle Tarball
+TAR_NAME=$(tar -tzf "$TARFILE" | head -1 | cut -f1 -d"/")
+
+tar -xzf "$TARFILE" -C "$DEST"
+DIR_PATH=$DEST/$TAR_NAME
+cd "$DIR_PATH"
+
+extract_file() {
+    component=$(basename "$1")
+    for file in "$1"/*
+    do
+        if tar -tf "$file" &>/dev/null; then
+            tar -xzf "$file" -C "$DIR_PATH/$component"
+            rm -rf "$file"
+        fi
+    done
+}
+
+# Extract the nested component specific tarfiles
+if [ "$COMPONENTS" == "all" ]; then
+    for entry in "$DIR_PATH"/*
+    do
+        extract_file "$entry"
+    done
+else
+    for entry in "$DIR_PATH"/*
+    do
+        component=$(basename "$entry")
+        for comps in $COMPONENTS
+        do
+            if [ "$component" == "$comps" ]; then
+                extract_file "$entry"
+            fi
+        done
+    done
+fi
+
+
+echo "Successfully Extracted the SB tarfile at path:$DEST !!!"
+
