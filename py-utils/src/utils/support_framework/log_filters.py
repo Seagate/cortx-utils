5451919308fc2cb1a1cb60e2a0a3ac84dfd4d6e8
Palak Garg <palak.garg@seagate.com>
2021-12-08 07:32:14 -0700

    EOS-25997: Implement limit_size API
    Signed-off-by: Palak Garg <palak.garg@seagate.com>

--- py-utils/src/utils/support_framework/log_filters.py
+++ py-utils/src/utils/support_framework/log_filters.py
@@ -17,6 +17,7 @@
 # please email opensource@seagate.com or cortx-questions@seagate.com.
 
 import os
+import time
 import shutil
 
 
@@ -24,13 +25,56 @@ class FilterLog:
     """Provides Filter interfaces for support bundle."""
 
     @staticmethod
-    def limit_size(src_dir, dest_dir, size, file_name_reg_ex):
-        # ToDo: Implementation of limit size filter.
-        # currently making a copy of source file into the dest_dir.
-        for file in os.listdir(src_dir):
-            if file.startswith(file_name_reg_ex):
-                shutil.copy(os.path.join(src_dir, file), dest_dir)
+    def get_size_in_bytes(size):
+        size_in_bytes = ''
+        units = [('GB', 1024**3),
+                ('MB', 1024**2),
+                ('KB', 1024),
+                ('B', 1)]
+        for suffix, multiplier in units:
+            if size.endswith(suffix):
+                num_units = size[:-len(suffix)]
+                size_in_bytes = int(float(num_units)) * multiplier
+                break
+        return size_in_bytes
 
+    @staticmethod
+    def truncate_file_size(src_dir, dest_dir, file_name,
+                           original_file_size, required_file_size):
+        """Truncate the Log size to the required file size and write the file to dest dir."""
+        shutil.copy(os.path.join(src_dir, file_name), dest_dir)
+        with open(os.path.join(dest_dir, file_name), "r+b") as reading, \
+                open(os.path.join(dest_dir, file_name), "r+b") as writing:
+            reading.seek(original_file_size - required_file_size)
+            writing.write(reading.read())
+        try:
+            os.truncate(os.path.join(dest_dir, file_name), required_file_size)
+        except OSError as error:
+            raise("Unable to truncate the file:", error)
+
+    @staticmethod
+    def limit_size(src_dir, dest_dir, size, file_name_reg_ex):
+        """Filter the log files in the source dir based on file size requested."""
+
+        list_of_files = filter(lambda f: os.path.isfile(os.path.join(src_dir, f)),
+                        os.listdir(src_dir))
+        # sort the files based on last modification time in descending order
+        list_of_files = sorted(list_of_files,
+                        key = lambda f: os.path.getmtime(os.path.join(src_dir, f)),
+                        reverse=True)
+        size_in_bytes = FilterLog.get_size_in_bytes(size.upper())
+        required_file_size = size_in_bytes
+        for file_name in list_of_files:
+            if file_name.startswith(file_name_reg_ex):
+                file_size = os.stat(os.path.join(src_dir, file_name)).st_size
+                if file_size <= required_file_size:
+                    shutil.copy(os.path.join(src_dir, file_name), dest_dir)
+                    required_file_size = size_in_bytes - file_size
+                elif file_size > required_file_size:
+                    # Truncate the file from the beginning to match the required file size
+                    FilterLog.truncate_file_size(src_dir, dest_dir, file_name,
+                        file_size, required_file_size)
+                    break
 
     @staticmethod
     def limit_time(src_dir, dest_dir, duration, file_name_reg_ex):
