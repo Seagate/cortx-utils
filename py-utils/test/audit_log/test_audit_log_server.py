4b50332783f06b147c56ddcc3764613386136826
Parayya Vastrad <56255712+vastradparayya@users.noreply.github.com>
2021-12-13 16:22:35 +0530

    EOS-26021: S3 Audit Log: Expose an API to push audit logs (#633)
    * Adding audit log server
    
    Signed-off-by: Parayya Vastrad <parayya.vastrad@seagate.com>
    
    * Codacy issues
    
    Signed-off-by: Parayya Vastrad <parayya.vastrad@seagate.com>
    
    * For error.pt makes use of UtilsError
    
    Signed-off-by: Parayya Vastrad <parayya.vastrad@seagate.com>
    
    * AuditLogError
    
    Signed-off-by: Parayya Vastrad <parayya.vastrad@seagate.com>
    
    * EOS-26021: fixed codacy and pr comments
    
    Signed-off-by: Rahul Tripathi <rahul.tripathi@seagate.com>
    
    Co-authored-by: Rahul Tripathi <78896910+rahul-tripathi-git@users.noreply.github.com>
    Co-authored-by: Rahul Tripathi <rahul.tripathi@seagate.com>

---
+++ py-utils/test/audit_log/test_audit_log_server.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+# CORTX Python common library.
+# Copyright (c) 2021 Seagate Technology LLC and/or its Affiliates
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+# For any questions about this software or licensing,
+# please email opensource@seagate.com or cortx-questions@seagate.com.
+
+
+import json
+import unittest
+import requests
+
+
+class TestAuditLog(unittest.TestCase):
+    """Test AuditLog rest server functionality."""
+    _base_url = 'http://127.0.0.1:28300/AuditLog/message/'
+
+    def test_send_audit_log(self):
+        """Send Audit log message."""
+        url = self._base_url
+        data = json.dumps({'messages': ['Hello', 'How are you?']})
+        headers = {'content-type': 'application/json'}
+        response = requests.post(url=url, data=data, headers=headers)
+        self.assertEqual(response.json()['status'], 'success')
+        self.assertEqual(response.status_code, 200)
+
+
+if __name__ == '__main__':
+    unittest.main()
