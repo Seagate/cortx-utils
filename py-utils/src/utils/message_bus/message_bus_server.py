a8ea13a60898c3c0db898e4636d984cffca5a907
Sachin Punadikar <sachin.punadikar@seagate.com>
2021-12-14 11:36:48 +0530

    Revert "MessageBus library code should not refer any conf file (#644)" (#646)
    This reverts commit 1500714a65939a3d8136d2dba491ecd4cd7eff92.

--- py-utils/src/utils/message_bus/message_bus_server.py
+++ py-utils/src/utils/message_bus/message_bus_server.py
@@ -21,10 +21,9 @@ from aiohttp import web
 from cortx.utils.utils_server import RestServer
 from cortx.utils.message_bus.error import MessageBusError
 from cortx.utils.utils_server.error import RestServerError
-from cortx.utils.message_bus import MessageConsumer, MessageProducer, MessageBus
+from cortx.utils.message_bus import MessageConsumer, MessageProducer
 from cortx.utils.log import Log
 from cortx.utils.common import CortxConf
-from cortx.utils.conf_store import Conf
 
 routes = web.RouteTableDef()
 
@@ -41,12 +40,9 @@ class MessageBusRequestHandler(RestServer):
             payload = await request.json()
             messages = payload['messages']
             cluster_conf = CortxConf.get_cluster_conf_path()
-            Conf.load('config', cluster_conf, skip_reload=True)
-            message_server_endpoints = Conf.get('config',\
-                    'cortx>external>kafka>endpoints')
-            MessageBus.init(message_server_endpoints=message_server_endpoints)
             producer = MessageProducer(producer_id='rest_producer', \
-                message_type=message_type, method='sync')
+                message_type=message_type, method='sync',\
+                cluster_conf=cluster_conf)
 
             producer.send(messages)
         except MessageBusError as e:
@@ -86,13 +82,9 @@ class MessageBusRequestHandler(RestServer):
             message_types = str(request.match_info['message_type']).split('&')
             consumer_group = request.rel_url.query['consumer_group']
             cluster_conf = CortxConf.get_cluster_conf_path()
-            Conf.load('config', cluster_conf, skip_reload=True)
-            message_server_endpoints = Conf.get('config',\
-                    'cortx>external>kafka>endpoints')
-            MessageBus.init(message_server_endpoints=message_server_endpoints)
             consumer = MessageConsumer(consumer_id='rest_consumer', \
                 consumer_group=consumer_group, message_types=message_types, \
-                auto_ack=True, offset='latest')
+                auto_ack=True, offset='latest', cluster_conf=cluster_conf)
 
             message = consumer.receive()
         except MessageBusError as e:
