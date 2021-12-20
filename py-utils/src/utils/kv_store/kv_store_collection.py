eab9904ab50e89e81f57241186bd55fb3cace731
SURYA KUMAR K <suryakumar.kumaravelan@seagate.com>
2021-12-20 18:33:18 +0530

    Set consul value strict with string (#657)
    Signed-off-by: suryakumar.kumaravelan <suryakumar.kumaravelan@seagate.com>

--- py-utils/src/utils/kv_store/kv_store_collection.py
+++ py-utils/src/utils/kv_store/kv_store_collection.py
@@ -433,7 +433,7 @@ class ConsulKvPayload(KvPayload):
 
     def _set(self, key: str, val: str, *args, **kwargs) -> Union[bool, None]:
         """ Set the value to the key in consul kv. """
-        return self._consul.kv.put(key, val)
+        return self._consul.kv.put(key, str(val))
 
     def _delete(self, key: str, *args, **kwargs) -> Union[bool, None]:
         """ Delete the key:value for the input key. """
