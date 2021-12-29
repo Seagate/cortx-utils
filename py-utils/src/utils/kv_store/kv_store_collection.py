f87646263d2a5dd74da0587ec9e90f41f3babecd
Udayan Yaragattikar <66424882+udayan-y21@users.noreply.github.com>
2021-12-29 08:47:53 +0530

    EOS-27052: ConfStore: accept parent key in consul url (#656)
    * EOS-22623: add openldap interface skeleton to generic db framework
    
    Implement store and reduced get (without object mapping).
    
    Signed-off-by: Alexander Voronov <alexander.voronov@seagate.com>
    
    * EOS-22623: add generic db example for OpenLdap
    
    Signed-off-by: Alexander Voronov <alexander.voronov@seagate.com>
    
    * EOS-22623: add object mapping to 'get' method for OpenLdap
    
    Signed-off-by: Alexander Voronov <alexander.voronov@seagate.com>
    
    * EOS-22626: implement filtering for OpenLdap queries
    
    Signed-off-by: Alexander Voronov <alexander.voronov@seagate.com>
    
    * EOS-22626: implement Update and Delete for OpenLdap
    
    Signed-off-by: Alexander Voronov <alexander.voronov@seagate.com>
    
    * EOS-22626: update the OpenLdap sample with filtering, update and deletion
    
    Signed-off-by: Alexander Voronov <alexander.voronov@seagate.com>
    
    * EOS-22626: handle TODOs for OpenLdap integration in generic db framework
    
    Signed-off-by: Alexander Voronov <alexander.voronov@seagate.com>
    
    * EOS-22626: fix codacy errors in OpenLdap sample application
    
    Signed-off-by: Alexander Voronov <alexander.voronov@seagate.com>
    
    * EOS-22632: implement 'count' mehtod for OpenLdap
    
    Signed-off-by: Alexander Voronov <alexander.voronov@seagate.com>
    
    * EOS-22632: remove manual extraction of result and description from LDAPError
    
    Signed-off-by: Alexander Voronov <alexander.voronov@seagate.com>
    
    * EOS-23899: update object when storing if it exists in OpenLdap
    
    Signed-off-by: Alexander Voronov <alexander.voronov@seagate.com>
    
    * Licence modifications
    
    * Accept Suffix in Consul URL for Confstore
    
    Co-authored-by: Alexander Voronov <alexander.voronov@seagate.com>

--- py-utils/src/utils/kv_store/kv_store_collection.py
+++ py-utils/src/utils/kv_store/kv_store_collection.py
@@ -410,19 +410,23 @@ class PillarStore(KvStore):
 class ConsulKvPayload(KvPayload):
     """ Backend adapter for consul api. """
 
-    def __init__(self, consul: Consul, delim: str = '>'):
+    def __init__(self, consul: Consul, store_path: str = '', delim: str = '>'):
         if len(delim) > 1:
             raise KvError(errno.EINVAL, "Invalid delim %s", delim)
         self._delim = delim
         self._data = {}
-        self._keys = []
         self._consul = consul
-        self.get_keys(self._keys)
-
+        self._store_path = store_path
+        if self._store_path:
+            if self._store_path.startswith('/'): self._store_path = self._store_path[1:]
+            if not self._store_path.endswith('/'): self._store_path = self._store_path + '/'
+        self._keys = self.get_keys()
 
     def get(self, key: str, *args, **kwargs) -> str:
         """ Get value for consul key. """
-        index, data = self._consul.kv.get(key)
+        if not key in self._keys:
+            return None
+        index, data = self._consul.kv.get(self._store_path + key)
         if isinstance(data, dict):
             if data['Value'] is None:
                 return ''
@@ -435,11 +439,11 @@ class ConsulKvPayload(KvPayload):
 
     def _set(self, key: str, val: str, *args, **kwargs) -> Union[bool, None]:
         """ Set the value to the key in consul kv. """
-        return self._consul.kv.put(key, str(val))
+        return self._consul.kv.put(self._store_path + key, str(val))
 
     def _delete(self, key: str, *args, **kwargs) -> Union[bool, None]:
         """ Delete the key:value for the input key. """
-        return self._consul.kv.delete(key)
+        return self._consul.kv.delete(self._store_path + key)
 
     def get_data(self, format_type: str = None, *args, **kwargs):
         """ Return a dict of kv pair. """
@@ -461,11 +465,15 @@ class ConsulKvPayload(KvPayload):
                     Invalid key: %s" % str(starts_with))
             else:
                 key_list =  self._consul.kv.get(
-                    starts_with, recurse=True, keys=True)[1]
+                    self._store_path+starts_with, recurse=True, keys=True)[1]
         else:
-            key_list =  self._consul.kv.get('', keys=True)[1]
+            key_list =  self._consul.kv.get(self._store_path, keys=True)[1]
         if key_list:
-            keys.extend(key_list)      
+            if self._store_path:
+                for each_key in key_list:
+                    keys.append(each_key.split(self._store_path)[1])
+            else:
+                keys.extend(key_list)
         return keys
 
     def search(self, parent_key: str, search_key: str, search_val: str) -> list:
@@ -499,11 +507,12 @@ class ConsulKVStore(KvStore):
         host, port = store_loc.split(':')
         try:
             self.c = Consul(host=host, port=port)
-            self._payload = ConsulKvPayload(self.c, self._delim)
+            self._payload = ConsulKvPayload(self.c, self._store_path, self._delim)
         except Exception as e:
             raise KvError(errno.ECONNREFUSED, "Connection refused." \
                 "Failed to eshtablish a new connection to consul endpoint: %s.\n %s", \
                 store_loc, e)
+
     def load(self, **kwargs) -> ConsulKvPayload:
         """ Return ConsulKvPayload object. """
         return self._payload
