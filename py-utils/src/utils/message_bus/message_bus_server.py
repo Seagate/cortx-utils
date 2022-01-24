e7a0cbb6bccbd46ad83b307d2cea92694bcf25b7
SURYA KUMAR K <suryakumar.kumaravelan@seagate.com>
2022-01-24 12:42:15 +0530

    EOS-27348: Updated Iem and MessageBus server cortxconf usage removed (#718)
    * Updated Iem and MessageBus server cortx conf changes
    
    Signed-off-by: suryakumar.kumaravelan <suryakumar.kumaravelan@seagate.com>
    
    * Added mappedconf class removed conf variable
    
    Signed-off-by: suryakumar.kumaravelan <suryakumar.kumaravelan@seagate.com>
    
    * code opt
    
    Signed-off-by: suryakumar.kumaravelan <suryakumar.kumaravelan@seagate.com>
    
    * Update utils_server.py
    
    Co-authored-by: Sachin Punadikar <sachin.punadikar@seagate.com>
    Signed-off-by: suryakumar.kumaravelan <suryakumar.kumaravelan@seagate.com>

--- py-utils/src/utils/message_bus/message_bus_server.py
+++ py-utils/src/utils/message_bus/message_bus_server.py
@@ -23,8 +23,6 @@ from cortx.utils.message_bus.error import MessageBusError
 from cortx.utils.utils_server.error import RestServerError
 from cortx.utils.message_bus import MessageConsumer, MessageProducer, MessageBus
 from cortx.utils.log import Log
-from cortx.utils.common import CortxConf
-from cortx.utils.conf_store import Conf
 
 routes = web.RouteTableDef()
 
@@ -40,14 +38,8 @@ class MessageBusRequestHandler(MessageServer):
             message_type = request.match_info['message_type']
             payload = await request.json()
             messages = payload['messages']
-            cluster_conf = CortxConf.get_cluster_conf_path()
-            Conf.load('config', cluster_conf, skip_reload=True)
-            message_server_endpoints = Conf.get('config',\
-                    'cortx>external>kafka>endpoints')
-            MessageBus.init(message_server_endpoints=message_server_endpoints)
             producer = MessageProducer(producer_id='rest_producer', \
                 message_type=message_type, method='sync')
-
             producer.send(messages)
         except MessageBusError as e:
             status_code = e.rc
@@ -85,15 +77,9 @@ class MessageBusRequestHandler(MessageServer):
         try:
             message_types = str(request.match_info['message_type']).split('&')
             consumer_group = request.rel_url.query['consumer_group']
-            cluster_conf = CortxConf.get_cluster_conf_path()
-            Conf.load('config', cluster_conf, skip_reload=True)
-            message_server_endpoints = Conf.get('config',\
-                    'cortx>external>kafka>endpoints')
-            MessageBus.init(message_server_endpoints=message_server_endpoints)
             consumer = MessageConsumer(consumer_id='rest_consumer', \
                 consumer_group=consumer_group, message_types=message_types, \
                 auto_ack=True, offset='latest')
-
             message = consumer.receive()
         except MessageBusError as e:
             status_code = e.rc
