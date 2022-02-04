d7c36211141b6a454308ea5911e9dd461348c0d1
Ujjwal Lanjewar <ujjwal.lanjewar@seagate.com>
2022-02-04 17:39:29 +0530

    New Task framework for Activity Tracking
    Signed-off-by: Ujjwal Lanjewar <ujjwal.lanjewar@seagate.com>

---
+++ py-utils/src/utils/task_framework/task_cli.py
@@ -0,0 +1,193 @@
+#!/usr/bin/env python3
+
+# CORTX-Py-Utils: CORTX Python common library.
+# Copyright (c) 2021 Seagate Technology LLC and/or its Affiliates
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+# For any questions about this software or licensing,
+# please email opensource@seagate.com or cortx-questions@seagate.com.
+
+import errno
+import argparse
+import inspect
+import sys
+import traceback
+from argparse import RawTextHelpFormatter
+from cortx.utils.task_store import Task
+from cortx.utils.task_store import TaskError
+from cortx.utils.schema import Format
+
+
+class TaskCli:
+    """ CLI for the Task Store """
+    _index = "Task_cli"
+
+    @staticmethod
+    def init(url: str):
+        """ Load TaskStore URL """
+        Task.init(url)
+
+    @staticmethod
+    def create(args):
+        """ Set Key Value """
+        if len(args.args) < 2:
+            raise TaskError(errno.EINVAL, "Insufficient args for create")
+        resource = args.args[0]
+        description = args.args[1]
+        task = Task.create(resource, description)
+        return task.id
+
+    @staticmethod
+    def update(args) -> str:
+        """ Obtain value for the given keys """
+        task_id = args.args[0]
+        pct_complete = args.args[1]
+        status = args.args[2]
+        task = Task.get(task_id)
+        Task.update(task, pct_complete, status)
+
+    @staticmethod
+    def start(args):
+        """ Deletes given set of keys from the Taskig """
+        task_id = args.args[0]
+        task = Task.get(task_id)
+        Task.start(task)
+
+    @staticmethod
+    def show(args):
+        """ Deletes given set of keys from the Taskig """
+        task_id = args.args[0]
+        task = Task.get(task_id)
+        return task.payload.json
+
+    @staticmethod
+    def search(args):
+        """ Deletes given set of keys from the Taskig """
+        resource_path = args.args[0]
+        filters = args.args[1].split(',')
+        task_list = Task.search(resource_path, filters)
+        return task_list
+
+    @staticmethod
+    def finish(args):
+        """ Returns list of keys present in store """
+        task_id = args.args[0]
+        task = Task.get(task_id)
+        Task.finish(task)
+
+
+class CreateCmd:
+    """ Create Cmd Structure """
+
+    @staticmethod
+    def add_args(sub_parser) -> None:
+        s_parser = sub_parser.add_parser('create', help=
+            "Create Task for the given resource\n"
+            "# task create '123>1>2>3'\n\n")
+        s_parser.set_defaults(func=TaskCli.create)
+        s_parser.add_argument('args', nargs='+', default=[], help='args')
+
+
+class StartCmd:
+    """ Set Cmd Structure """
+
+    @staticmethod
+    def add_args(sub_parser) -> None:
+        s_parser = sub_parser.add_parser('start', help=
+            "Starts the given Task. Example command:\n"
+            "# task start '123>1>2>3>222.222'\n\n")
+        s_parser.set_defaults(func=TaskCli.start)
+        s_parser.add_argument('args', nargs='+', default=[], help='args')
+
+
+class FinishCmd:
+    """ Finish Cmd Structure """
+
+    @staticmethod
+    def add_args(sub_parser) -> None:
+        s_parser = sub_parser.add_parser('finish', help=
+            "Marks the given Task complete. Example command:\n"
+            "# task finish '123>1>2>3>222.222'\n\n")
+        s_parser.set_defaults(func=TaskCli.finish)
+        s_parser.add_argument('args', nargs='+', default=[], help='args')
+
+
+class UpdateCmd:
+    """ Updarte Cmd Structure """
+
+    @staticmethod
+    def add_args(sub_parser) -> None:
+        s_parser = sub_parser.add_parser('update', help=
+            "Updates the given Task complete. Example command:\n"
+            "# task update '123>1>2>3>222.222 50 'reading...''\n\n")
+        s_parser.set_defaults(func=TaskCli.update)
+        s_parser.add_argument('args', nargs='+', default=[], help='args')
+
+class SearchCmd:
+    """ Updarte Cmd Structure """
+
+    @staticmethod
+    def add_args(sub_parser) -> None:
+        s_parser = sub_parser.add_parser('search', help=
+            "Lists the Tasks for given resource. Example command:\n"
+            "# task search '123>1>2>3>'\n\n")
+        s_parser.set_defaults(func=TaskCli.search)
+        s_parser.add_argument('args', nargs='+', default=[], help='args')
+
+class ShowCmd:
+    """ Updarte Cmd Structure """
+
+    @staticmethod
+    def add_args(sub_parser) -> None:
+        s_parser = sub_parser.add_parser('show', help=
+            "Shows the Task information. Example command:\n"
+            "# task show '123>1>2>3>222.222'\n\n")
+        s_parser.set_defaults(func=TaskCli.show)
+        s_parser.add_argument('args', nargs='+', default=[], help='args')
+
+
+def main():
+    # Setup Parser
+    parser = argparse.ArgumentParser(description='Task CLI',
+        formatter_class=RawTextHelpFormatter)
+    parser.add_argument('url', help='URL for the TaskStore backend')
+    sub_parser = parser.add_subparsers(title='command',
+        help="represents the action e.g. create, start, update, finish", 
+            dest='command')
+
+    # Add Command Parsers
+    members = inspect.getmembers(sys.modules[__name__])
+    for name, cls in members:
+        if name != "Cmd" and name.endswith("Cmd"):
+            cls.add_args(sub_parser)
+
+    # Parse and Process Arguments
+    try:
+        args = parser.parse_args()
+        TaskCli.init(args.url)
+        out = args.func(args)
+        if out is not None and len(out) > 0:
+            print(out)
+        return 0
+
+    except TaskError as e:
+        sys.stderr.write("%s\n\n" % str(e))
+        sys.stderr.write("%s\n" % traceback.format_exc())
+        return e.rc
+
+    except Exception as e:
+        sys.stderr.write("%s\n\n" % str(e))
+        sys.stderr.write("%s\n" % traceback.format_exc())
+        return errno.EINVAL
+
+if __name__ == "__main__":
+    rc = main()
+    sys.exit(rc)
---
+++ py-utils/src/utils/task_framework/task_store.py
@@ -0,0 +1,140 @@
+#!/bin/python3
+
+# CORTX Python common library.
+# Copyright (c) 2020 Seagate Technology LLC and/or its Affiliates
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+# For any questions about this software or licensing,
+# please email opensource@seagate.com or cortx-questions@seagate.com.
+
+import errno
+import time
+import json
+import re
+
+from cortx.template import Singleton
+from cortx.utils.task_store.error import TaskError
+from cortx.utils.kv_store.kv_store import KvStoreFactory
+from cortx.utils.kv_store import KvPayload
+
+
+class TaskEntry:
+    """ Represents System Activity """
+
+    def __init__(self, *args, **kwargs): 
+        self._payload = KvPayload()
+        task_id = kwargs.get('id')
+        if task_id is None:
+            resource_path = kwargs.get('resource_path')
+            description = kwargs.get('description')
+            if resource_path is None or description is None:
+                raise TaskError(errno.EINVAL, "Missing resource_path/description")
+
+            self._id = f'{resource_path}>%s' %time.time()
+            self._payload['id'] = self._id
+            self._payload['resource_path'] = resource_path
+            self._payload['description'] = description
+            self._payload['pct_complete'] = 0
+        else:
+            self._id = task_id
+            for key, value in kwargs.items():
+                self._payload[key] = value
+            
+
+    @property
+    def id(self) -> str:
+        return self._id
+
+    @property
+    def payload(self) -> dict:
+        return self._payload
+
+    def set_attr(self, attr: str, val: str):
+        self._payload[attr] = val
+
+    def set_status(self, pct_complete: int, status: str):
+        self._payload['pct_complete'] = pct_complete
+        self._payload['status'] = status
+
+    def start(self):
+        self._payload['start_time'] = time.time()
+
+    def finish(self, status: str = ""):
+        self._payload['pct_complete'] = 100
+        self._payload['finish_time'] = time.time()
+        self._payload['status'] = status
+
+
+class Task(metaclass=Singleton):
+    """ Represent Task Framework. Singleton Class """
+    _kv_store = None
+
+    @staticmethod
+    def init(backend_url):
+        """ Initializes the backend for the Task Store """
+        Task._kv_store = KvStoreFactory.get_instance(backend_url)
+
+    @staticmethod
+    def create(resource_path: str, description: str):
+        """ Creates a task for the given resource """
+        task = TaskEntry(resource_path=resource_path, description=description)
+        Task._kv_store.set([task.id], [task.payload.json])
+        return task
+
+    @staticmethod
+    def start(task: TaskEntry):
+        """ Start the task. Records the current time as the start time """
+        if not isinstance(task, TaskEntry):
+            raise TaskError(errno.EINVAL, "start(): Invalid arg %s", task)
+        task.start()
+        Task._kv_store.set([task.id], [task.payload.json])
+
+    @staticmethod
+    def finish(task: TaskEntry):
+        """ Completes a task. Records current time as the completion time """
+        task.finish()
+        Task._kv_store.set([task.id], [task.payload.json])
+
+    @staticmethod
+    def update(task: TaskEntry, pct_complete: int, status: str):
+        """ Updates the task progress """
+        task.set_status(pct_complete, status)
+        Task._kv_store.set([task.id], [task.payload.json])
+
+    @staticmethod
+    def search(resource_path: str, filters: list) -> list:
+        """ Searches for a task as per given criteria """
+        task_list = Task._kv_store.get_keys(resource_path)
+        out_list = []
+        for task_id in task_list:
+            val = Task._kv_store.get([task_id])
+            data = json.loads(val[0])
+            matched = True
+            for f in filters:
+                key_spec = re.split("[^a-zA-Z_]", f)
+                if key_spec[0] not in data.keys():
+                    matched = False
+                    break
+                vars()[key_spec[0]] = data[key_spec[0]]
+                if not eval(f):
+                    matched = False
+                    break
+            if matched:
+                out_list.append(task_id)
+        return out_list
+                
+    @staticmethod
+    def get(task_id: str):
+        val = Task._kv_store.get([task_id])       
+        if len(val) == 0 or val[0] is None: 
+            raise TaskError(errno.EINVAL, "get(): invalid task id %s", task_id)
+        data = json.loads(val[0])
+        return TaskEntry(**data)
