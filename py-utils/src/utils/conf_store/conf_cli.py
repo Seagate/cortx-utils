9c7fd08ec26ed8ccf15478a0cb489d9d88c90ce5
rohit-k-dwivedi <83213072+rohit-k-dwivedi@users.noreply.github.com>
2022-02-08 10:31:56 +0530

    EOS-28501: Fix INI section issue (#728)
    * Fix INI section issue
    
    Signed-off-by: Ujjwal Lanjewar <ujjwal.lanjewar@seagate.com>
    
    * patch to avoid multi level keys ini formatt
    
    Signed-off-by: Rohit Dwivedi <rohit.k.dwivedi@seagate.com>
    
    * code convention patch
    
    Signed-off-by: Rohit Dwivedi <rohit.k.dwivedi@seagate.com>
    
    Co-authored-by: Ujjwal Lanjewar <ujjwal.lanjewar@seagate.com>

--- py-utils/src/utils/conf_store/conf_cli.py
+++ py-utils/src/utils/conf_store/conf_cli.py
@@ -131,6 +131,14 @@ class ConfCli:
             Conf.save(ConfCli._index)
 
     @staticmethod
+    def copy(args):
+        """Copy One or more Keys to the target config url"""
+        key_list = None if len(args.args) < 1 else args.args[0].split(';')
+        target_index = 'target'
+        ConfCli.load(args.target_url, target_index)
+        Conf.copy(ConfCli._index, target_index, key_list)
+
+    @staticmethod
     def get_keys(args, index: str = None) -> list:
         """ Returns list of keys present in store """
         key_index = 'true' if args.key_index == None else args.key_index.lower().strip()
@@ -198,6 +206,21 @@ class SetCmd:
         s_parser.add_argument('-d', dest='kv_delim',
             help="Delimiter for k=v (default is '=')")
         s_parser.add_argument('args', nargs='+', default=[], help='args')
+
+
+class CopyCmd:
+    """ Copy Cmd Structure """
+
+    @staticmethod
+    def add_args(sub_parser) -> None:
+        s_parser = sub_parser.add_parser('copy', help=
+            "Copy Keys or whole config to the target config url.\n"
+            "Example command(s):\n"
+            "# conf json:///tmp/csm.conf copy json:///tmp/csm1.conf\n"
+            "# conf json:///tmp/csm.conf copy json:///tmp/csm1.conf 'k1>k2;k3'\n\n")
+        s_parser.set_defaults(func=ConfCli.copy)
+        s_parser.add_argument('target_url', help='target url for copy')
+        s_parser.add_argument('args', nargs='*', default=[], help='args')
 
 
 class DeleteCmd:
