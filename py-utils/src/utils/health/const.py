c1314d6797bfda7dc02f571b5f7a43fb583b8e0e
gauri.bhosale <gauri.bhosale@seagate.com>
2022-02-20 09:45:29 -0700

    CORTX-27956
    Addresses comments:
    1. change 2022 for copyright year
    2. add event dict in event object
    3. removed _ on event_id
    4. rename function name to set_upload
    
    Signed-off-by: gauri.bhosale <gauri.bhosale@seagate.com>

--- py-utils/src/utils/health/const.py
+++ py-utils/src/utils/health/const.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3.6
 
 # CORTX-Py-Utils: CORTX Python common library.
-# Copyright (c) 2020 Seagate Technology LLC and/or its Affiliates
+# Copyright (c) 2022 Seagate Technology LLC and/or its Affiliates
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published
 # by the Free Software Foundation, either version 3 of the License, or
@@ -36,9 +36,9 @@ class HEALTH_EVENT_ATTRIBUTES:
     NODE_ID = "node_id"
     RESOURCE_TYPE = "resource_type"
     RESOURCE_ID = "resource_id"
-    RESOURCE_STATUS = "event_type"
+    RESOURCE_STATUS = "resource_status"
     SPECIFIC_INFO = "specific_info"
 
 
-HEALTH_EVENT_PAYLOAD = "payload"
 HEALTH_EVENT_HEADER = "header"
+HEALTH_EVENT_PAYLOAD = "payload"
--- py-utils/src/utils/health/event.py
+++ py-utils/src/utils/health/event.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3.6
 
 # CORTX-Py-Utils: CORTX Python common library.
-# Copyright (c) 2020 Seagate Technology LLC and/or its Affiliates
+# Copyright (c) 2022 Seagate Technology LLC and/or its Affiliates
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published
 # by the Free Software Foundation, either version 3 of the License, or
@@ -14,7 +14,8 @@
 # along with this program. If not, see <https://www.gnu.org/licenses/>.
 # For any questions about this software or licensing,
 # please email opensource@seagate.com or cortx-questions@seagate.com.
-from const import HEALTH_EVENT_ATTRIBUTES
+from const import HEALTH_EVENT_ATTRIBUTES, HEALTH_EVENT_HEADER, \
+    HEALTH_EVENT_PAYLOAD
 import time
 import uuid
 
@@ -22,41 +23,49 @@ VERSION = "1.0"
 
 
 class Event:
+
     """
     Class for health status event message schema,
     where producer can import this class object and add it
     in there existing schema.
     """
+
     def __init__(self):
         """
         Initialize default value for header and payload component.
         """
-        self.header = {}
-        self.header[HEALTH_EVENT_ATTRIBUTES.VERSION] = VERSION
-        self.header[HEALTH_EVENT_ATTRIBUTES.TIMESTAMP] = str(int(time.time()))
-        self.header[HEALTH_EVENT_ATTRIBUTES.EVENT_ID] =  \
-            self.header[HEALTH_EVENT_ATTRIBUTES.TIMESTAMP] + \
-            "_" + str(uuid.uuid4().hex)
+        self.event = {}
+        header = {}
+        header[HEALTH_EVENT_ATTRIBUTES.VERSION] = VERSION
+        header[HEALTH_EVENT_ATTRIBUTES.TIMESTAMP] = str(int(time.time()))
+        header[HEALTH_EVENT_ATTRIBUTES.EVENT_ID] =  \
+            header[HEALTH_EVENT_ATTRIBUTES.TIMESTAMP] + \
+            str(uuid.uuid4().hex)
+        self.event[HEALTH_EVENT_HEADER] = header
 
-        self.payload = {}
-        self.payload[HEALTH_EVENT_ATTRIBUTES.SOURCE] = None
-        self.payload[HEALTH_EVENT_ATTRIBUTES.CLUSTER_ID] = None
-        self.payload[HEALTH_EVENT_ATTRIBUTES.SITE_ID] = None
-        self.payload[HEALTH_EVENT_ATTRIBUTES.RACK_ID] = None
-        self.payload[HEALTH_EVENT_ATTRIBUTES.STORAGESET_ID] = None
-        self.payload[HEALTH_EVENT_ATTRIBUTES.NODE_ID] = None
-        self.payload[HEALTH_EVENT_ATTRIBUTES.RESOURCE_TYPE] = None
-        self.payload[HEALTH_EVENT_ATTRIBUTES.RESOURCE_ID] = None
-        self.payload[HEALTH_EVENT_ATTRIBUTES.RESOURCE_STATUS] = None
-        self.payload[HEALTH_EVENT_ATTRIBUTES.SPECIFIC_INFO] = {}
+        payload = {}
+        payload[HEALTH_EVENT_ATTRIBUTES.SOURCE] = None
+        payload[HEALTH_EVENT_ATTRIBUTES.CLUSTER_ID] = None
+        payload[HEALTH_EVENT_ATTRIBUTES.SITE_ID] = None
+        payload[HEALTH_EVENT_ATTRIBUTES.RACK_ID] = None
+        payload[HEALTH_EVENT_ATTRIBUTES.STORAGESET_ID] = None
+        payload[HEALTH_EVENT_ATTRIBUTES.NODE_ID] = None
+        payload[HEALTH_EVENT_ATTRIBUTES.RESOURCE_TYPE] = None
+        payload[HEALTH_EVENT_ATTRIBUTES.RESOURCE_ID] = None
+        payload[HEALTH_EVENT_ATTRIBUTES.RESOURCE_STATUS] = None
+        payload[HEALTH_EVENT_ATTRIBUTES.SPECIFIC_INFO] = {}
+        self.event[HEALTH_EVENT_PAYLOAD] = payload
 
-    def set_or_update_payload(self, _input):
+    def set_payload(self, _input):
         """
-        Update payload values
+        Update payload values.
+        params: payload data of attributes with value type : dict
+        _input = {"source" : "hare", "node_id" : "2"}
         """
         for key in _input.keys():
-            if key in list(self.payload.keys()):
-                self.payload[key] = _input[key]
+            if key in list(self.event[HEALTH_EVENT_PAYLOAD].keys()):
+                self.event[HEALTH_EVENT_PAYLOAD][key] = _input[key]
             else:
-                self.payload[HEALTH_EVENT_ATTRIBUTES.SPECIFIC_INFO][key] \
+                specific_info = HEALTH_EVENT_ATTRIBUTES.SPECIFIC_INFO
+                self.event[HEALTH_EVENT_PAYLOAD][specific_info][key] \
                     = _input[key]
