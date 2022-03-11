c65ac06fe9ac57651299efa119c80fcbf66c8f78
Vidya Kabber <vidya.kabber@seagate.com>
2022-03-11 17:30:41 +0530

    Read endpoint from cluster.conf
    Signed-off-by: Vidya Kabber <vidya.kabber@seagate.com>
    Signed-off-by: suryakumar.kumaravelan <suryakumar.kumaravelan@seagate.com>

--- py-utils/test/conf_store/test_conf_store_conf.py
+++ py-utils/test/conf_store/test_conf_store_conf.py
@@ -62,16 +62,16 @@ class TestConfStore(unittest.TestCase):
 
         for index_url in load_index_url():
             index = index_url[0]
-            url = index_url[1]
+            url = endpoint_key = index_url[1]
             if index not in TestConfStore.indexes:
                 cls.indexes.append(index)
 
             if 'consul' in index.lower():
-                endpoint_url = load_consul_endpoint(index_url[1], cls.cluster_conf_path)
+                endpoint_url = load_consul_endpoint(endpoint_key, cls.cluster_conf_path)
                 if endpoint_url is not None:
                     url = endpoint_url.replace('http', 'consul')
                 else:
-                    LOGGER.error(f'\nInvalid consul endpoint key : {index_url[1]}\n')
+                    LOGGER.error(f'\nInvalid consul endpoint key : {endpoint_key}\n')
 
             load_config(index, url)
 
