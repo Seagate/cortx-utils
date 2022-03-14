7b7bdf59aeee99bef189c03080c023c54fd9f64a
Vidya Kabber <vidya.kabber@seagate.com>
2022-03-14 17:38:23 +0530

    'CORTX-29208'
    Signed-off-by: Vidya Kabber <vidya.kabber@seagate.com>
    Signed-off-by: suryakumar.kumaravelan <suryakumar.kumaravelan@seagate.com>

--- py-utils/test/conf_store/test_conf_store_conf.py
+++ py-utils/test/conf_store/test_conf_store_conf.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+# !/usr/bin/env python3
 
 # CORTX Python common library.
 # Copyright (c) 2021 Seagate Technology LLC and/or its Affiliates
@@ -17,107 +17,106 @@
 # please email opensource@seagate.com or cortx-questions@seagate.com.
 import yaml
 import unittest
+import logging
 import os
 from cortx.utils.kv_store import KvStoreFactory
 from cortx.utils.conf_store import Conf
 
-dir_path = os.path.dirname(os.path.abspath('.'))
-url_config_file = os.path.join(dir_path, 'conf_store', 'config.yaml')
-print(dir_path)
-print(url_config_file)
+LOGGER = logging.getLogger(__name__)
 
-def test_current_file(file_path):
-    kv_store = KvStoreFactory.get_instance(file_path)
-    data = kv_store.load()
-    print("-----------------kv_store------------------", kv_store)
-    print("-----------------data------------------", data)
-    return [kv_store, data]
+dir_path = os.path.dirname(os.path.realpath(__file__))
+url_config_file = os.path.join(dir_path, 'config.yaml')
+
 
 def load_index_url():
     """ Load index and url from config file. """
     with open(url_config_file) as fd:
         urls = yaml.safe_load(fd)['conf_url_list']
-    for url_index in urls.keys():
-        if 'consul' in url_index:
-            yield [url_index, urls[url_index]]
+    for url_index in urls:
+        yield [url_index, urls[url_index]]
+
+
+def load_config(index, backend_url):
+    """ Instantiate and Load Config into constore. """
+    Conf.load(index, backend_url)
+
 
 def load_consul_endpoint(endpoint_key, cluster_conf_url):
     Conf.load('config', cluster_conf_url)
     endpoint_url = Conf.get('config', endpoint_key)
     return endpoint_url
 
-class TestStore(unittest.TestCase):
+
+class TestConfStore(unittest.TestCase):
+    """ Test confstore backend urls mentioned in config file. """
+
+    indexes = []
     _cluster_conf_path = ''
     loaded_consul = ''
 
     @classmethod
     def setUpClass(cls, cluster_conf_path: str = 'yaml:///etc/cortx/cluster.conf'):
         """ Setup test class. """
-        if TestStore._cluster_conf_path:
-            cls.cluster_conf_path = TestStore._cluster_conf_path
+        if TestConfStore._cluster_conf_path:
+            cls.cluster_conf_path = TestConfStore._cluster_conf_path
         else:
             cls.cluster_conf_path = cluster_conf_path
 
-        index_url = load_index_url()
-        url = endpoint_key = index_url[1]
-        endpoint_url = load_consul_endpoint(endpoint_key, cls.cluster_conf_path)
-        if endpoint_url is not None and 'http' in endpoint_url:
-            url = endpoint_url.replace('http', 'consul')
-        else:
-            LOGGER.error(f'\nInvalid consul endpoint key : {endpoint_key}\n')
+        for index_url in load_index_url():
+            index = index_url[0]
+            url = endpoint_key = index_url[1]
+            if index not in TestConfStore.indexes:
+                cls.indexes.append(index)
 
-        TestStore.loaded_consul = test_current_file(url)
-        print("-------------loaded_consul--------------", TestStore.loaded_consul)
+            endpoint_url = load_consul_endpoint(endpoint_key, cls.cluster_conf_path)
+            if endpoint_url is not None and 'http' in endpoint_url:
+                url = endpoint_url.replace('http', 'consul')
+            else:
+                LOGGER.error(f'\nInvalid consul endpoint key : {endpoint_key}\n')
 
-    def test_consul_a_set_get_kv(self):
-        """ Test consul kv set and get a KV. """
-        TestStore.loaded_consul[0].set(['consul_cluster_uuid'], ['#410'])
-        out = TestStore.loaded_consul[0].get(['consul_cluster_uuid'])
-        self.assertEqual('#410', out[0])
+        load_config(index, url)
 
-    def test_consul_b_query_unknown_key(self):
-        """ Test consul kv query for an absent key. """
-        out = TestStore.loaded_consul[0].get(['Wrong_key'])
-        self.assertIsNone(out[0])
+        def test_set_and_get(self):
+            """ Set and get the value for a key. """
+            for index in TestConfStore.indexes:
+                Conf.set(index, 'test_key1', 'test_value1')
+                get_val = Conf.get(index, 'test_key1')
+                self.assertEqual('test_value1', get_val)
 
-    def test_consul_store_c_set_nested_key(self):
-        """ Test consul kv set a nested key. """
-        TestStore.loaded_consul[0].set(['consul_cluster>uuid'], ['#411'])
-        out = TestStore.loaded_consul[0].get(['consul_cluster>uuid'])
-        self.assertEqual('#411', out[0])
+        def test_get_keys(self):
+            """ set keys and get keys """
+            key_list = ['test_k1', 'test_k2', 'test_k3', 'test_k4']
+            for index in TestConfStore.indexes:
+                for key in key_list:
+                    Conf.set(index, key, '#!random_value')
+                get_key_list = Conf.get_keys(index)
+                self.assertTrue(all([True if key in get_key_list else False for key in key_list]))
 
-    def test_consul_store_d_set_multiple_kv(self):
-        """ Test consul kv by setting nested key structure """
-        TestStore.loaded_consul[0].set(['cloud>cloud_type', 'kafka>message_type'],
-            ['Azure', 'receive'])
-        out1 = TestStore.loaded_consul[0].get(['kafka>message_type'])
-        out2 = TestStore.loaded_consul[0].get(['cloud>cloud_type'])
-        self.assertEqual('receive', out1[0])
-        self.assertEqual('Azure', out2[0])
+        def test_get_keys_starts_with(self):
+            """set and get keys which starts with a string. """
+            key_list = ['swtest_k1', 'swtest_k2', 'swtest_k3', 'swtest_k4']
+            for index in TestConfStore.indexes:
+                for key in key_list:
+                    Conf.set(index, key, '#!random_value')
+                get_key_list = Conf.get_keys(index, starts_with='swtest')
+                self.assertTrue(all([True if key in get_key_list else False for key in key_list]))
 
-    def test_consul_store_e_delete_kv(self):
-        """ Test consul kv by removing given key using delete api """
-        TestStore.loaded_consul[0].delete(['cloud>cloud_type'])
-        out = TestStore.loaded_consul[0].get(['cloud>cloud_type'])
-        self.assertEqual([None], out)
+        def test_get_wrong_key(self):
+            """ Get a wrong key. """
+            for index in TestConfStore.indexes:
+                val = Conf.get(index, 'Wrong_key')
+                self.assertEqual(val, None)
 
-    def test_consul_store_f_set_value_null(self):
-        """Test consul kv by setting empty string as value."""
-        TestStore.loaded_consul[0].set(['test'],[''])
-        out = TestStore.loaded_consul[0].get(['test'])
-        TestStore.loaded_consul[0].delete(['test'])
-        self.assertEqual([''], out)
+        def test_set_delete_and_get(self):
+            """ Get a deleted key. """
+            for index in TestConfStore.indexes:
+                Conf.set(index, 'K1', 'V1')
+                Conf.delete(index, 'K1')
+                val = Conf.get(index, 'K1')
+                self.assertEqual(val, None)
 
-    def test_consul_store_g_set_search(self):
-        """Test consul search."""
-        TestStore.loaded_consul[0].set(['test>child_key>leaf_key'],['value'])
-        out = TestStore.loaded_consul[0].search('test', 'leaf_key', 'value')
-        TestStore.loaded_consul[0].delete(['test>child_key>leaf_key'])
-        self.assertEqual(['test>child_key>leaf_key'], out)
-
-
-if __name__ == '__main__':
-    import sys
-    if len(sys.argv) >= 2:
-        TestStore._cluster_conf_path = sys.argv.pop()
-    unittest.main()
+    if __name__ == '__main__':
+        import sys
+        if len(sys.argv) >= 2:
+            TestConfStore._cluster_conf_path = sys.argv.pop()
+        unittest.main()
