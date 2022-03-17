3e71bce6a4d1a5a0426f6f3a6a8d9e409d0dd0d4
Rahul Tripathi <rahul.tripathi@seagate.com>
2022-03-17 02:24:58 -0600

    implemented Retry Class decorator
    Signed-off-by: Rahul Tripathi <rahul.tripathi@seagate.com>

--- py-utils/src/utils/common/common.py
+++ py-utils/src/utils/common/common.py
@@ -1,40 +1,40 @@
 import os
 import errno
+import time
+from functools import wraps
 
 from cortx.utils.conf_store import Conf
 from cortx.utils.conf_store.error import ConfError
 
-def retry(ExceptionToCheck, tries=4, delay=3, backoff=2):
-    """Retry calling the decorated function using an exponential backoff.
-	
-    :param ExceptionToCheck: the exception to check. may be a tuple of
-        exceptions to check
-    :type ExceptionToCheck: Exception or tuple
-    :param tries: number of times to try (not retry) before giving up
-    :type tries: int
-    :param delay: initial delay between retries in seconds
-    :type delay: int
-    :param backoff: backoff multiplier e.g. value of 2 will double the delay
-        each retry
-    :type backoff: int
-    """
-    def deco_retry(f):
+class Retry:
+    def __init__(self, exception, tries=4, delay=1, backoff=2):
+        self._exception = exception
+        self._tries = tries
+        self._delay = delay
+        self._backoff = backoff
 
-        @wraps(f)
-        def f_retry(*args, **kwargs):
-            mtries, mdelay = tries, delay
-            while mtries > 1:
-                try:
-                    return f(*args, **kwargs)
-                except ExceptionToCheck:
-                    time.sleep(mdelay)
-                    mtries -= 1
-                    mdelay *= backoff
-            return f(*args, **kwargs)
+    def __call__(self, func):
+        if len(func) == 1:
+            print(self._tries, self._exception)
+            print (func)
+            func = func[0]
+            def deco_retry(func):
 
-        return f_retry  # true decorator
-
-    return deco_retry
+                @wraps(func)
+                def wrap(*x,**y):
+                    mtries, mdelay = self._tries, self._delay
+                    while mtries > 1:
+                        try:
+                            return func(*x,**y)
+                        except self._exception:
+                            time.sleep(mdelay)
+                            mtries -= 1
+                            mdelay *= self._backoff
+                    return func(*x,**y)
+                return wrap
+            return deco_retry
+        else:
+            raise Exception
 
 class CortxConf:
     _index = 'config_file'
